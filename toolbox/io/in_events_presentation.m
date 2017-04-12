7733713b4670ef4347c1ccee340107b1d1c9ac74
ftadel <francois.tadel@mcgill.ca>
2017-04-12 16:26:43 +0200

    Fixed Presentation .log reader

--- toolbox/io/in_events_presentation.m
+++ toolbox/io/in_events_presentation.m
@@ -26,25 +26,52 @@ fid = fopen(EventFile, 'r');
 if (fid < 0)
     error('Cannot open marker file.');
 end
-% Skip first line
-tmp = fgetl(fid);
-% Read file
-mrk = textscan(fid, '%*d %*s %s %d');
+% Inialize event list
+mrkType = {};
+mrkTime = [];
+isHeader = 1;
+% Loop to skip the first comment lines
+while 1
+    % Read one line
+    strLine = fgetl(fid);
+    % Reached the end of the file: return
+    if isequal(strLine, -1)
+        break;
+    end
+    % The line is empty
+    if isempty(strLine)
+        continue;
+    end
+    % The line must contain the column names (eg. "Trial") before we start reading values
+    if isHeader
+        if ~isempty(strfind(strLine, 'Trial'))
+            isHeader = 0;
+        end
+        continue;
+    end
+    % Split line
+    cellLine = str_split(strLine, sprintf(' \t'));
+    % If the line contains enough entries: use it
+    if (length(cellLine) >= 4) && ~isempty(str2num(cellLine{4}))
+        mrkType{end+1} = cellLine{3};
+        mrkTime(end+1) = str2num(cellLine{4});
+    end
+end
 % Close file
 fclose(fid);
 
 % List of events
-uniqueEvt = unique(mrk{1});
+uniqueEvt = unique(mrkType);
 % Initialize returned structure
 events = repmat(db_template('event'), [1, length(uniqueEvt)]);
 % Create events list
 for iEvt = 1:length(uniqueEvt)
     % Find all the occurrences of event #iEvt
-    iMrk = find(strcmpi(mrk{1}, uniqueEvt{iEvt}));
+    iMrk = find(strcmpi(mrkType, uniqueEvt{iEvt}));
     % Add event structure
     events(iEvt).label   = uniqueEvt{iEvt};
     events(iEvt).epochs  = ones(1, length(iMrk));
-    events(iEvt).times   = double(mrk{2}(iMrk)') .* 1e-4;
+    events(iEvt).times   = double(mrkTime(iMrk)) .* 1e-4;
     events(iEvt).samples = round(events(iEvt).times .* sFile.prop.sfreq);
     events(iEvt).reactTimes  = [];
     events(iEvt).select      = 1;
