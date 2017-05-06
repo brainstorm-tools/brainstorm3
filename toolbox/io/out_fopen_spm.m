5c91e7f0de5ac04177594eb30f5870b34bcbcaec
ftadel <francois.tadel@mcgill.ca>
2017-05-06 13:24:30 +0200

    Use all channels when no obvious modality is available

--- toolbox/io/out_fopen_spm.m
+++ toolbox/io/out_fopen_spm.m
@@ -80,6 +80,10 @@ if ~isempty(iMeg)
     error(['MEG sensors are currently not supported by this function.' 10 ...
            'Please contact us through the Brainstorm user forum to request this feature.']);
 end
+% If all the channels have other types: consider it's all EEG
+if isempty(iEeg) && isempty(iMeg)
+    iEeg = 1:length(ChannelMat.Channel);
+end
 % Sensors
 for i = 1:length(iEeg)
     if ~isempty(ChannelMat.Channel(iEeg(i)).Loc) && ~all(ChannelMat.Channel(iEeg(i)).Loc(:) == 0)
