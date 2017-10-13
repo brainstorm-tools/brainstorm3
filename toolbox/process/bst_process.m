8d94056cff07aee2905f155edda961ee943fd37f
ftadel <francois.tadel@mcgill.ca>
2017-10-13 11:36:12 +0200

    Remove unused process options when calling from a script

--- toolbox/process/bst_process.m
+++ toolbox/process/bst_process.m
@@ -2106,6 +2106,19 @@ function [OutputFiles, OutputFiles2] = CallProcess(sProcess, sInputs, sInputs2, 
     if ~isempty(sInputs2) && ~isstruct(sInputs2)
         sInputs2 = GetInputStruct(sInputs2);
     end
+    % Remove the options that are not matching the type of the inputs
+    optNames = fieldnames(sProcess.options);
+    for iOpt = 1:length(optNames)
+        if ~isfield(sProcess.options.(optNames{iOpt}), 'InputTypes')
+            continue;
+        elseif ismember(sInputs(1).FileType, sProcess.options.(optNames{iOpt}).InputTypes)
+            continue;
+        elseif isempty(sInputs2) && ismember(sInputs2(1).FileType, sProcess.options.(optNames{iOpt}).InputTypes)
+            continue;
+        else
+            sProcess.options = rmfield(sProcess.options, optNames{iOpt});
+        end
+    end
     % Get options
     for i = 1:2:length(varargin)
         if ~ischar(varargin{i})
