93ac006fc05c89ee8d75733e098cee4b0fc799e5
ftadel <francois.tadel@mcgill.ca>
2017-11-29 18:32:29 +0100

    Updated automatic selection of SEEG channels

--- external/ImaGIN/io/ImaGIN_select_channels.m
+++ external/ImaGIN/io/ImaGIN_select_channels.m
@@ -66,8 +66,13 @@ if isSEEG
     for i = 1:length(uniqueTags)
         % Get channels of this tag
         iTag = find(strcmpi(uniqueTags{i}, AllTags));
-        % Remove if more than 18 (except for Salpetriere electrodes with digits in the name, and except 'X' used by )
-        if ((length(iTag) > 18) && (any(iTag < 10) || any(iTag >= 30)))
+        % If the tag is "X": Used by Nihon Kohden for technical tracks, but can be a SEEG electrode too. Keep only indices <= 18
+        if isequal(uniqueTags{i}, 'X')
+            iTagExtra = iTag(AllInd(iTag) > 18);
+            AllNames(iTagExtra) = {'XXXXX'};
+            AllTags(iTagExtra) = {'XXXXX'};
+        % Remove if more than 18 (except for Salpetriere electrodes with digits in the name)
+        elseif ((length(iTag) > 18) && (any(iTag < 10) || any(iTag >= 30)))
             AllNames(iTag) = {'XXXXX'};
             AllTags(iTag) = {'XXXXX'};
         end
@@ -89,8 +94,11 @@ for i = 1:length(AllNames)
     % Does not contain at least a letter
     elseif isSEEG && ~any(ismember(lower(AllNames{i}), 'abcdefghijklmnopqrstuvwxyz'))
         continue;
+    % Contains a /: exclude
+    elseif isSEEG && any(ismember(lower(chNames{i}), '/'))
+        continue;
     % Unwanted labels
-    elseif ismember(lower(AllTags{i}), {'xxxxx', 'mark', 'dc', 'emg', 'eog', 'veo', 'heo', 'veog', 'heog', 'myo', 'myog', 'myod', 'dd', 'dg', 'el', 'ref', 'eegref', 'eref', 'vref', 'ref', 'pulse', 'mast', 'spo2', 'lpar', 'rpar', 'tib', '/'})
+    elseif ismember(lower(AllTags{i}), {'xxxxx', 'mark', 'dc', 'emg', 'eog', 'veo', 'heo', 'veog', 'heog', 'myo', 'myog', 'myod', 'dd', 'dg', 'el', 'ref', 'eegref', 'eref', 'vref', 'ref', 'pulse', 'mast', 'spo2', 'lpar', 'rpar', 'tib'})
         % MAYBE ADD 'oc' ??
         continue;
     % Unwanted labels
@@ -103,21 +111,21 @@ for i = 1:length(AllNames)
     elseif isSEEG && ismember(AllNames{i}, {'sFp1','sFp2','sF4','sF3','sC3','sC4','sP4','sP3','sO2','sO1','sF8','sF7','sT8/T4','sT7/T3','sP8/T6','sP7/T5','sPz','sFz','sIO1','sIO2','sAF9','sAF10','sF9','sF10','sCB1','sCB2','sTP7','sTP9','sTP10','sTP8','sOz','sIz','sPO4','sPO3','sCP5','sCP6','sCP1','sCP2','sFT9','sFT10','sFC2','sFC1','sAF3','sAF4','sFC6','sFC5','sCPz','sP1','sPOz','sP2','sP6','sC6','sP5','sC1','sC2','sC5','sF2','sF6','sF1','sAF8','sF5','sAF7','sFpz','sFCz','sCz'})
         continue;
     % Unwanted FPx/Fx/Cx/Tx/Pz/Ox labels
-    elseif isSEEG && ismember(lower(AllNames{i}), {'fp1','fp2'}) && ~any(ismember({'fp3','fp4'}, lower(AllNames)))
+    elseif isSEEG && ismember(lower(AllNames{i}), {'fp1','fp2'}) && ~any(ismember({'fp3','fp4','fp5','fp6','fp9','fp10','fp11','fp12','fp13','fp14','fp15','fp16'}, lower(AllNames)))
         continue;
-    elseif isSEEG && ismember(lower(AllNames{i}), {'f3','f4','f7','f8'}) && ~any(ismember({'f1','f2'}, lower(AllNames)))
+    elseif isSEEG && ismember(lower(AllNames{i}), {'f3','f4','f7','f8'}) && ~any(ismember({'f1','f2','f5','f6','f9','f10','f11','f12','f13','f14','f15','f16'}, lower(AllNames)))
         continue;
-    elseif isSEEG && ismember(lower(AllNames{i}), {'ft1','ft2','ft9','ft10'}) && ~any(ismember({'ft5','ft6'}, lower(AllNames)))
+    elseif isSEEG && ismember(lower(AllNames{i}), {'ft1','ft2','ft9','ft10'}) && ~any(ismember({'ft5','ft6','ft11','ft12','ft13','ft14','ft15','ft16'}, lower(AllNames)))
         continue;
-    elseif isSEEG && ismember(lower(AllNames{i}), {'tp9','tp10'}) && ~any(ismember({'tp1','tp2'}, lower(AllNames)))
+    elseif isSEEG && ismember(lower(AllNames{i}), {'tp9','tp10'}) && ~any(ismember({'tp1','tp2','tp5','tp6','tp11','tp12','tp13','tp14','tp15','tp16'}, lower(AllNames)))
         continue;
-    elseif isSEEG && ismember(lower(AllNames{i}), {'c3','c4'}) && ~any(ismember({'c1','c2'}, lower(AllNames)))
+    elseif isSEEG && ismember(lower(AllNames{i}), {'c3','c4'}) && ~any(ismember({'c1','c2','c5','c6','c7','c8','c9','c10','c11','c12','c13','c14','c15','c16'}, lower(AllNames)))
         continue;
-    elseif isSEEG && ismember(lower(AllNames{i}), {'t3','t4','t5','t6','t9','t10'}) && ~any(ismember({'t1','t2'}, lower(AllNames)))
+    elseif isSEEG && ismember(lower(AllNames{i}), {'t3','t4','t5','t6','t9','t10'}) && ~any(ismember({'t1','t2','t7','t8','t11','t12','t13','t14','t15','t16'}, lower(AllNames)))
         continue;
-    elseif isSEEG && ismember(lower(AllNames{i}), {'p3','p4'}) && ~any(ismember({'p1','p2'}, lower(AllNames)))
+    elseif isSEEG && ismember(lower(AllNames{i}), {'p3','p4'}) && ~any(ismember({'p1','p2','p5','p6','p7','p8','p9','p10','p11','p12','p13','p14','p15','p16'}, lower(AllNames)))
         continue;
-    elseif isSEEG && ismember(lower(AllNames{i}), {'o1','o2'}) && ~any(ismember({'o3','o4'}, lower(AllNames)))
+    elseif isSEEG && ismember(lower(AllNames{i}), {'o1','o2'}) && ~any(ismember({'o3','o4','o4','o5','o6','o7','o8','o9','o10','o11','o12','o13','o14','o15','o16'}, lower(AllNames)))
         continue;
     % Otherwise: accept
     else
