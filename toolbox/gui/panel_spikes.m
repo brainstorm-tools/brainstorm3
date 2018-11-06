d7faec79d3d14a12ee593ce00fb647040915ca74
ftadel <francois.tadel@mcgill.ca>
2018-11-06 16:51:08 +0100

    ephys toolbox: updated standard comments in file headers

--- toolbox/gui/panel_spikes.m
+++ toolbox/gui/panel_spikes.m
@@ -5,7 +5,7 @@ function varargout = panel_spikes(varargin)
 %
 % @=============================================================================
 % This function is part of the Brainstorm software:
-% http://neuroimage.usc.edu/brainstorm
+% https://neuroimage.usc.edu/brainstorm
 % 
 % Copyright (c)2000-2018 University of Southern California & McGill University
 % This software is distributed under the terms of the GNU General Public License
