889fd7c6d02cddcca1db2ee26ae056d4a19408e5
ftadel <francois.tadel@mcgill.ca>
2019-05-21 13:42:44 +0200

    Support for compiled FieldTrip+SPM for 2016a to 2017b

--- toolbox/io/in_trials_nwb.m
+++ toolbox/io/in_trials_nwb.m
@@ -1,4 +1,25 @@
 function [sFile, nEpochs] = in_trials_nwb(sFile, nwb2)
+% IN_TRIALS_NWB Read trials from a Neurodata Without Borders .nwb file.
+
+% @=============================================================================
+% This function is part of the Brainstorm software:
+% https://neuroimage.usc.edu/brainstorm
+% 
+% Copyright (c)2000-2019 University of Southern California & McGill University
+% This software is distributed under the terms of the GNU General Public License
+% as published by the Free Software Foundation. Further details on the GPLv3
+% license can be found at http://www.gnu.org/copyleft/gpl.html.
+% 
+% FOR RESEARCH PURPOSES ONLY. THE SOFTWARE IS PROVIDED "AS IS," AND THE
+% UNIVERSITY OF SOUTHERN CALIFORNIA AND ITS COLLABORATORS DO NOT MAKE ANY
+% WARRANTY, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO WARRANTIES OF
+% MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, NOR DO THEY ASSUME ANY
+% LIABILITY OR RESPONSIBILITY FOR THE USE OF THIS SOFTWARE.
+%
+% For more information type "brainstorm license" at command prompt.
+% =============================================================================@
+%
+% Authors: Konstantinos Nasiotis, 2019
 
 % Check if there are trials present
 if isempty(nwb2.intervals_trials)
