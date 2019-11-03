brainstorm-tools/brainstorm3

commit 6463c542080f38cf0f51f638e4efec3a2baddd31

Bug fix: Loading files with empty nAvg and Leff fields (set to 1 by default)

toolbox/io/in_bst_results.m
@@ -265,7 +265,7 @@ end
 
 %% ===== FILL OTHER MISSING FIELDS =====
 for i = 1:length(FieldsToRead)
-    if ~isfield(Results, FieldsToRead{i})
+    if ~isfield(Results, FieldsToRead{i}) || isempty(Results.(FieldsToRead{i}))
         switch(FieldsToRead{i}) 
             case 'Time'
                 Results.Time = [1 2];

doc/license.html
@@ -3,7 +3,7 @@
 
 </head>
 <body alink="#fff000" link="#fff000" vlink="#fff000">
-<!-- LICENCE_START -->Version: 3.191029 (29-Oct-2019)<br>
+<!-- LICENCE_START -->Version: 3.191030 (30-Oct-2019)<br>
 <span style="font-style: italic;">COPYRIGHT &copy; 2000-2019
 USC &amp; McGill University.<br>
 </span>

toolbox/io/in_bst_data.m
@@ -94,7 +94,7 @@ end
 
 % ===== MISSING FIELDS =====
 for i = 1:length(FieldsToRead)
-    if ~isfield(DataMat, FieldsToRead{i})
+    if ~isfield(DataMat, FieldsToRead{i}) || isempty(DataMat.(FieldsToRead{i}))
         switch(FieldsToRead{i})
             case 'DataType'
                 DataMat.DataType = 'recordings';

toolbox/io/in_bst_matrix.m
@@ -70,7 +70,7 @@ end
 
 %% ===== FILL OTHER MISSING FIELDS =====
 for i = 1:length(FieldsToRead)
-    if ~isfield(sMat, FieldsToRead{i})
+    if ~isfield(sMat, FieldsToRead{i}) || isempty(sMat.(FieldsToRead{i}))
         switch(FieldsToRead{i}) 
             case 'nAvg'
                 sMat.(FieldsToRead{i}) = 1;

toolbox/io/in_bst_timefreq.m
@@ -78,7 +78,7 @@ end
 
 %% ===== FILL OTHER MISSING FIELDS =====
 for i = 1:length(FieldsToRead)
-    if ~isfield(Timefreq, FieldsToRead{i})
+    if ~isfield(Timefreq, FieldsToRead{i}) || isempty(Timefreq.(FieldsToRead{i}))
         switch(FieldsToRead{i}) 
             case 'Measure'
                 Timefreq.(FieldsToRead{i}) = 'none';

commit 4941d2d708d82be650cc3a6d51226ead2c28a060

Bug fix: Correct bug introduced in https://github.com/brainstorm-tools/brainstorm3/commit/6463c542080f38cf0f51f638e4efec3a2baddd31

toolbox/io/in_bst_results.m
@@ -268,7 +268,9 @@ for i = 1:length(FieldsToRead)
     if ~isfield(Results, FieldsToRead{i}) || isempty(Results.(FieldsToRead{i}))
         switch(FieldsToRead{i}) 
             case 'Time'
-                Results.Time = [1 2];
+                if ~isfield(Results, FieldsToRead{i})   % Only if time is not defined - we want to keep it empty for kernels
+                    Results.Time = [1 2];
+                end
             case 'nComponents'
                 if ~isempty(strfind(ResultsFile, '_unconstr'))
                     Results.nComponents = 3;