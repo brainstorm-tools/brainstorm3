9816e432b5086c7f410ebe3b840eaa0193290fbc
Konstantinos <knasiwtis@hotmail.com>
2020-02-20 14:05:06 -0500

    incomplete phase locking visualization
    Just opens up the TF visualization for now

--- toolbox/gui/view_timefreq.m
+++ toolbox/gui/view_timefreq.m
@@ -58,7 +58,8 @@ end
 
 if ~isempty(strfind(lower(TimefreqFile), 'spike_field_coherence')) ...
         || ~isempty(strfind(lower(TimefreqFile), 'noise_correlation')) ...
-        || ~isempty(strfind(lower(TimefreqFile), 'rasterplot'))
+        || ~isempty(strfind(lower(TimefreqFile), 'rasterplot')) ...
+        || ~isempty(strfind(lower(TimefreqFile), 'spiking_phase_locking'))
     isEphysFile = 1;
     GlobalData.UserFrequencies.HideFreqPanel = 1;
 else
