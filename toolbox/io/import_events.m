13a353cfcfb5c90526f782350cc49e3b86d8e402
juangpc <juangpc@gmail.com>
2020-04-21 13:48:21 -0500

    remove reference to graph2 in import_events.m

--- toolbox/io/import_events.m
+++ toolbox/io/import_events.m
@@ -121,8 +121,6 @@ if isempty(newEvents)
             newEvents = in_events_neuroscan(sFile, EventFile);
         case 'GRAPH'
             newEvents = in_events_graph(sFile, EventFile);
-        case 'GRAPH2'
-            newEvents = in_events_graph2(sFile, EventFile);
         case 'TRL'
             newEvents = in_events_trl(sFile, EventFile);
         case 'KIT'
