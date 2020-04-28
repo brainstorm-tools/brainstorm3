4a330d985a02c171f00d0b93b24685c63d3c2383
Edouard <edouard.delaire@icloud.com>
2020-04-28 13:38:48 +0200

    add nirstorm function

--- toolbox/io/in_fopen_snirf.m
+++ toolbox/io/in_fopen_snirf.m
@@ -25,50 +25,47 @@ function  [DataMat, ChannelMat] = in_fopen_snirf(DataFile)
 % Authors: Edouard Delaire (2020)
     jnirs       = loadsnirf(DataFile);
     
-        
+    time        = jnirs.nirs.data.time;
+    nb_time     = length(time);
+    wavelengths = jnirs.nirs.probe.wavelengths;
+
     
-    
-    time=jnirs.nirs.data.time;
-    nb_time=length(time);
-    wavelengths=jnirs.nirs.probe.wavelengths;
-    % Read probe setting 
-    
-    n_det=size(jnirs.nirs.probe.detectorPos,1);
-    n_source=size(jnirs.nirs.probe.sourcePos,1);
-    
-    src_pos=jnirs.nirs.probe.sourcePos;
-    det_pos=jnirs.nirs.probe.detectorPos;
+    if isfield( jnirs.nirs.probe, 'sourcePos3D') && isfield( jnirs.nirs.probe, 'detectorPos3D')
+        src_pos     = jnirs.nirs.probe.sourcePos3D;
+        det_pos     = jnirs.nirs.probe.detectorPos3D;
+    else
+        src_pos     = jnirs.nirs.probe.sourcePos;
+        det_pos     = jnirs.nirs.probe.detectorPos;
+    end    
     
     % Read measurment list 
-    nb_channels = size(jnirs.nirs.data.measurementList,2);
-    nb_aux      = length(jnirs.nirs.aux);
-    ChannelMat = db_template('channelmat');
-    ChannelMat.Comment = 'NIRS-BRS channels';
+    nb_channels         = size(jnirs.nirs.data.measurementList,2);
+    nb_aux              = length(jnirs.nirs.aux);
+    ChannelMat          = db_template('channelmat');
+    ChannelMat.Comment  = 'NIRS-BRS channels';
     
-    Channels(nb_channels+nb_aux)=struct('Type','NIRS','Name','','Loc',[], ...
+    Channels(nb_channels+nb_aux) = struct('Type','NIRS','Name','','Loc',[], ...
                                          'Orient',[],'Weight',1,'Comment',[],...
                                          'Group','');
                                         
     for i_chan=1:nb_channels
         % this assume measure are raw; need to change for Hbo,HbR,HbT
-        channel=jnirs.nirs.data.measurementList(i_chan);
-        
-        measure_tag = sprintf('WL%d', round(wavelengths(channel.wavelengthIndex)));
-        
-        
-        Channels(i_chan).Name       = sprintf('S%dD%d%s', channel.sourceIndex, ...
-                                         channel.detectorIndex, measure_tag);
+        channel     = jnirs.nirs.data.measurementList(i_chan);
+
+        Channels(i_chan).Name       =  nst_format_channel(channel.sourceIndex, channel.detectorIndex, wavelengths(channel.wavelengthIndex)); 
+       
         Channels(i_chan).Type       = 'NIRS';
         Channels(i_chan).Weight     = 1;
+        
         Channels(i_chan).Loc(:,1)   = src_pos(channel.sourceIndex  ,:);
         Channels(i_chan).Loc(:,2)   = det_pos(channel.detectorIndex,:);
         Channels(i_chan).Group      = measure_tag;
     end
     
     for i_aux=1:nb_aux
-        channel=jnirs.nirs.aux(i_aux);
-        Channels(nb_channels+i_aux).Type='NIRS_AUX';
-        Channels(nb_channels+i_aux).Name=channel.name;
+        channel     = jnirs.nirs.aux(i_aux);
+        Channels(nb_channels+i_aux).Type = 'NIRS_AUX';
+        Channels(nb_channels+i_aux).Name = fix_str(channel.name);
         
         % Sanity check : make sure that time course are consistent with
         % nirs one (compare channel.time with nirs timecourse)
@@ -91,32 +88,32 @@ function  [DataMat, ChannelMat] = in_fopen_snirf(DataFile)
     
     % Read fiducials
     if isfield( jnirs.nirs.probe, 'landmarkLabels')
-        n_landmark=size(jnirs.nirs.probe.landmarkPos,1);
+        n_landmark = size(jnirs.nirs.probe.landmarkPos,1);
         
         for i_landmark=1:n_landmark
-            name=strrep(jnirs.nirs.probe.landmarkLabels(:,:,i_landmark),' ','');
-            coord=jnirs.nirs.probe.landmarkPos(i_landmark,:);
+            name = fix_str(jnirs.nirs.probe.landmarkLabels(:,:,i_landmark));
+            coord= jnirs.nirs.probe.landmarkPos(i_landmark,:);
             switch name
                 case 'Nasion'
                     ChannelMat.SCS.NAS = coord;
                     ChannelMat.HeadPoints.Loc(:, end+1) = coord';
-                    ChannelMat.HeadPoints.Label{end+1} = name;
-                    ChannelMat.HeadPoints.Type{end+1}  = 'CARDINAL';
+                    ChannelMat.HeadPoints.Label{end+1}  = name;
+                    ChannelMat.HeadPoints.Type{end+1}   = 'CARDINAL';
                 case 'LeftEar'
                     ChannelMat.SCS.LPA = coord;
                     ChannelMat.HeadPoints.Loc(:, end+1) = coord';
-                    ChannelMat.HeadPoints.Label{end+1} = name;
-                    ChannelMat.HeadPoints.Type{end+1}  = 'CARDINAL';
+                    ChannelMat.HeadPoints.Label{end+1}  = name;
+                    ChannelMat.HeadPoints.Type{end+1}   = 'CARDINAL';
                     
                 case 'RightEar'    
                     ChannelMat.SCS.RPA = coord;
                     ChannelMat.HeadPoints.Loc(:, end+1) = coord';
-                    ChannelMat.HeadPoints.Label{end+1} = name;
-                    ChannelMat.HeadPoints.Type{end+1}  = 'CARDINAL';
+                    ChannelMat.HeadPoints.Label{end+1}  = name;
+                    ChannelMat.HeadPoints.Type{end+1}   = 'CARDINAL';
                 otherwise
                     ChannelMat.HeadPoints.Loc(:, end+1) = coord';
-                    ChannelMat.HeadPoints.Label{end+1} = name;
-                    ChannelMat.HeadPoints.Type{end+1}  = 'EXTRA';
+                    ChannelMat.HeadPoints.Label{end+1}  = name;
+                    ChannelMat.HeadPoints.Type{end+1}   = 'EXTRA';
             end    
         end           
     end    
@@ -128,25 +125,81 @@ function  [DataMat, ChannelMat] = in_fopen_snirf(DataFile)
     data(1:nb_channels,:)=jnirs.nirs.data.dataTimeSeries';
     for i_aux=1:nb_aux % add auxilary data
         data(nb_channels+i_aux,:)=jnirs.nirs.aux(i_aux).dataTimeSeries';
-    end    
+    end
+    
+    % Read Stim 
+    
+    
+    
     
     %% ===== FILL STRUCTURE =====
     % Initialize returned file structure                    
     DataMat =  db_template('DataMat');                    
 
     % Add information read from header
-    [fPath, fBase, fExt] = bst_fileparts(DataFile);
-    DataMat.Comment     = fBase;
-    DataMat.filename    = DataFile;
+    [fPath, fBase, fExt]    = bst_fileparts(DataFile);
+    DataMat.Comment         = fBase;
+    DataMat.filename        = DataFile;
     
     
-    DataMat.format      = 'NIRS-BRS';
-    DataMat.DataType    = 'recordings';
-    DataMat.device      = 'Unknown';
-    DataMat.F           = data; 
-    DataMat.Time        = time;
+    DataMat.format          = 'NIRS-BRS';
+    DataMat.DataType        = 'recordings';
+    DataMat.Device          = 'Unknown';
+    DataMat.F               = data; 
+    DataMat.Time            = time;
 
-    DataMat.channelflag = ones(nb_channels + nb_aux, 1); % GOOD=1; BAD=-1;
+    DataMat.ChannelFlag = ones(nb_channels + nb_aux, 1); % GOOD=1; BAD=-1;
 
+end
+
+function str = fix_str(str)
+
+    if size(str,1) > size(str,2) % change from column to line
+        str=str';
+    end    
+    
+    %remove extra space at the end
+    str=strrep(str,' ','');
+end
+
+function channel_label = nst_format_channel(isrc, idet, measure)
+% NST_FORMAT_CHANNEL make channel label from source, dectector and measure information.
+%
+%   CHANNEL_LABEL = NST_FORMAT_CHANNEL(ISRC, IDET, MEAS)
+%
+%        ISRC (int >= 0): source index
+%        IDET (int >= 0): extracted detector index
+%        MEAS (int | str): measure value. 
+%                          Either wavelength (int) or Hb type (str)
+%                          -> 'HbO', 'HbR', 'HbT'
+%
+%       CHANNEL_LABEL (str): 
+%           formatted as 'SxDyWLz' or 'SxDyHbt', where:
+%               x: source index
+%               y: detector index
+%               z: wavelength
+%               t: Hb type (O, R, T).
+%           Examples: S1D2WL685, S3D01HbR
+%
+%   See also NST_UNFORMAT_CHANNEL
+
+% stub:
+assert(isrc >= 0);
+assert(idet >= 0);
+
+if nargin >= 3
+    assert(isnumeric(measure) || (ischar(measure) && ...
+        ismember(measure, {'HbO', 'HbR', 'HbT'})));
+    
+    if isnumeric(measure)
+        assert(measure >= 0);
+        assert(round(measure) == measure);
+        measure = sprintf('WL%d', measure);
+    end
+else
+    measure = '';
+end
+
+channel_label = sprintf('S%dD%d%s', isrc, idet, measure);
 end
 
