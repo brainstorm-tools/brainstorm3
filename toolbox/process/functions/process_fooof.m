4901036cc8ec97b21964ee36cbd6e5797b3a7964
ftadel <francois.tadel@mcgill.ca>
2020-09-03 10:05:37 +0200

    Updated version number

--- toolbox/process/functions/process_fooof.m
+++ toolbox/process/functions/process_fooof.m
@@ -7,14 +7,14 @@ function varargout = process_fooof(varargin)
 %    BioRxiv, 299859. doi: https://doi.org/10.1101/299859
 
 % @=============================================================================
-% This software is part of the Brainstorm software:
-% http://neuroimage.usc.edu/brainstorm
-%
-% Copyright (c)2000-2020 Brainstorm by the University of Southern California
+% This function is part of the Brainstorm software:
+% https://neuroimage.usc.edu/brainstorm
+% 
+% Copyright (c)2000-2020 University of Southern California & McGill University
 % This software is distributed under the terms of the GNU General Public License
-% as published by the Free Software Foundation. Further details on the GPL
+% as published by the Free Software Foundation. Further details on the GPLv3
 % license can be found at http://www.gnu.org/copyleft/gpl.html.
-%
+% 
 % FOR RESEARCH PURPOSES ONLY. THE SOFTWARE IS PROVIDED "AS IS," AND THE
 % UNIVERSITY OF SOUTHERN CALIFORNIA AND ITS COLLABORATORS DO NOT MAKE ANY
 % WARRANTY, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO WARRANTIES OF
