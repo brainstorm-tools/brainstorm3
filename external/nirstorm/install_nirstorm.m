fa1217a663738531366d4dd0785e3299396093a7
Francois <francois.tadel@mcgill.ca>
2020-11-08 10:47:41 +0100

    Reformatting

--- external/nirstorm/install_nirstorm.m
+++ external/nirstorm/install_nirstorm.m
@@ -1,32 +1,36 @@
 function [status,err] = install_nirstorm(extra,mode, tmp_folder)
-% Download and install NISTORM in Brainstorm
-% Input : 
-% Extra: option to install extra feature from nirstorm (debug, wip)
-%  - debug for developping tool, wip for work in progress feature
-% mode: mode of installation; either copy or link. Copy recommended for non
-% develloper. 
-% tmp_folder, download location. Usefull only for developper using the link
-% option. 
+% INSTALL_NIRSTORM Download and install NIRSTORM in Brainstorm
+% 
+% INPUTS: 
+%    - extra: cell array, list of extra features to install from nirstorm ('debug', 'wip')
+%       - 'debug': install developping tool
+%       - 'wip': install work in progress features
+%    - mode: String, installation mode:
+%       - 'copy': recommended for non-developpers
+%       - 'link': 
+%    - tmp_folder: Download location. Useful only for developper using the link option
+%
+% Authors: Edouard Delaire, 2020
 
-err= {};
+err = {};
 status = 1;
 nistorm_url = 'https://github.com/Nirstorm/nirstorm/archive/master.zip';
 
-if nargin < 1
-   extra={}; 
+if (nargin < 1) || isempty(extra)
+   extra = {}; 
 end
-if nargin < 2
-   mode='copy'; 
+if (nargin < 2) || isempty(mode)
+   mode = 'copy'; 
 end
-if nargin < 3
+if (nargin < 3) || isempty(tmp_folder)
    tmp_folder = bst_get('BrainstormTmpDir');
 end
 
-nistorm_folder=fullfile(tmp_folder,'nirstorm-master');
+nirstorm_folder = fullfile(tmp_folder,'nirstorm-master');
 try 
     unzip(nistorm_url,tmp_folder);
 catch
-   err='Unable to download nirstorm';
+   err = 'Unable to download nirstorm';
    status = 0; 
    return;
 end
@@ -34,7 +38,7 @@ end
 addpath(nistorm_folder);
 
 try 
-    nst_install(mode,extra,nistorm_folder )
+    nst_install(mode,extra,nistorm_folder);
 catch ME
     err = ME.message;
     status = 0;
@@ -43,10 +47,10 @@ end
 
 if strcmp(mode,'copy')
     rmpath(nistorm_folder)
-    rmpath( fullfile(nistorm_folder, 'dist_tools'));
+    rmpath(fullfile(nistorm_folder, 'dist_tools'));
     
     [status,msg] = rmdir(nistorm_folder, 's');
-    err= msg;
+    err = msg;
 end
 end
 
