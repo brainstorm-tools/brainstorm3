84736dc1ea4d088676c279f22c204ea5bce5319b
Raymundo Cassani <raymundo.cassani@gmail.com>
2021-04-16 17:45:41 -0400

    Add field for db_get calls

--- toolbox/db/db_add.m
+++ toolbox/db/db_add.m
@@ -162,17 +162,17 @@ if ismember(fileType, {'subjectimage', 'channel', 'noisecov', 'ndatacov'})
 %                 delfile = bst_fullfile(ProtocolInfo.SUBJECTS, sSubject.Anatomy(1).FileName);
 %             end
         case 'channel'
-            sFile = db_get('FunctionalFile', sqlConn, struct('Study', iTarget, 'Type', 'channel'));
+            sFile = db_get('FunctionalFile', sqlConn, struct('Study', iTarget, 'Type', 'channel'), 'Filename');
             if ~isempty(sFile)
                 delfile = bst_fullfile(ProtocolInfo.STUDIES, sFile.FileName);
             end
         case 'noisecov'
-            sFile = db_get('FunctionalFile', sqlConn, struct('Study', iTarget, 'Type', 'noisecov'));
+            sFile = db_get('FunctionalFile', sqlConn, struct('Study', iTarget, 'Type', 'noisecov'), 'Filename');
             if ~isempty(sFile)
                 delfile = bst_fullfile(ProtocolInfo.STUDIES, sFile(1).FileName);
             end
         case 'ndatacov'
-            sFile = db_get('FunctionalFile', sqlConn, struct('Study', iTarget, 'Type', 'ndatacov'));
+            sFile = db_get('FunctionalFile', sqlConn, struct('Study', iTarget, 'Type', 'ndatacov'), 'Filename');
             if ~isempty(sFile)
                 delfile = bst_fullfile(ProtocolInfo.STUDIES, sFile(1).FileName);
             end
