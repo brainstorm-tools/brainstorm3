0010d28489b644e0724d2577761b5061715ab025
ftadel <francois.tadel@mcgill.ca>
2021-04-21 11:07:20 +0200

    Anatomy: SPM Segment: Enable T1+T2 segmentation

--- toolbox/process/functions/process_mni_normalize.m
+++ toolbox/process/functions/process_mni_normalize.m
@@ -2,8 +2,8 @@ function varargout = process_mni_normalize( varargin )
 % PROCESS_MNI_NORMALIZE: Compute deformation fields to the MNI ICBM152 space.
 %
 % USAGE:     OutputFiles = process_mni_normalize('Run', sProcess, sInputs)
-%         [isOk, errMsg] = process_mni_normalize('Compute',            MriFile, Method)
-%                          process_mni_normalize('ComputeInteractive', MriFile, Method=[ask], isUnload=1)
+%         [isOk, errMsg] = process_mni_normalize('Compute',            T1File, Method)
+%                          process_mni_normalize('ComputeInteractive', T1File, Method=[ask], isUnload=1)
 
 % @=============================================================================
 % This function is part of the Brainstorm software:
@@ -23,7 +23,7 @@ function varargout = process_mni_normalize( varargin )
 % For more information type "brainstorm license" at command prompt.
 % =============================================================================@
 %
-% Authors: Francois Tadel, 2020
+% Authors: Francois Tadel, 2020-2021
 
 eval(macro_method);
 end
@@ -54,6 +54,10 @@ function sProcess = GetDescription()
          'maff8', 'segment'};
     sProcess.options.method.Type    = 'radio_label';
     sProcess.options.method.Value   = 'maff8';
+    % Use T2 when available
+    sProcess.options.uset2.Comment = 'Use T2 when available ("segment" only)';
+    sProcess.options.uset2.Type    = 'checkbox';
+    sProcess.options.uset2.Value   = 0;
 end
 
 
@@ -76,6 +80,8 @@ function OutputFiles = Run(sProcess, sInputs)
     end
     % Get method
     Method = sProcess.options.method.Value;
+    % Use T2
+    UseT2 = sProcess.options.uset2.Value;
       
     % ===== GET SUBJECT =====
     % Get subject 
@@ -89,12 +95,27 @@ function OutputFiles = Run(sProcess, sInputs)
         bst_report('Error', sProcess, [], ['No MRI available for subject "' SubjectName '".']);
         return
     end
-    
+    % Get default MRI as T1
+    T1File = sSubject.Anatomy(sSubject.iAnatomy).FileName;
+    % Look for T2 MRI
+    T2File = [];
+    if UseT2 && (length(sSubject.Anatomy) > 1)
+        iT2 = find(~cellfun(@(c)isempty(strfind(c,'t2')), lower({sSubject.Anatomy.Comment})));
+        % Warning when multiple T2 images
+        if (length(iT2) > 1)
+            bst_report('Warning', sProcess, [], ['Subject "' sSubject.Name '" has multiple anatomy volumes with tag "T2": not using T2 to avoid confusion.' 10 ...
+                'To use a T2 MRI for better volume segmentation, rename or delete the extra T2-labelled files from the subject anatomy.']);
+            iT2 = [];
+        end
+        % Get T2 filename
+        if ~isempty(iT2)
+            T2File = sSubject.Anatomy(iT2).FileName;
+        end
+    end
+
     % ===== COMPUTE MNI TRANSFORMATION =====
-    % Get default MRI
-    MriFile = sSubject.Anatomy(sSubject.iAnatomy).FileName;
     % Call normalize function
-    [sMri, errMsg] = Compute(MriFile, Method);
+    [sMriT1, errMsg] = Compute(T1File, Method, T2File);
     % Error handling
     if ~isempty(errMsg)
         bst_report('Error', sProcess, [], errMsg);
@@ -105,14 +126,22 @@ end
 
 
 %% ===== COMPUTE =====
-function [sMri, errMsg] = Compute(MriFile, Method)
-    [sMri, errMsg] = bst_normalize_mni(MriFile, Method);
+function [sMriT1, errMsg] = Compute(T1File, Method, T2File)
+    % Parse inputs
+    if (nargin < 3) || isempty(T2File)
+        T2File = [];
+    end
+    % Compute normalization
+    [sMriT1, errMsg] = bst_normalize_mni(T1File, Method, T2File);
 end
 
 
 %% ===== COMPUTE/INTERACTIVE =====
-function sMri = ComputeInteractive(MriFile, Method, isUnload)
+function sMriT1 = ComputeInteractive(T1File, Method, isUnload, T2File)
     % Parse inputs
+    if (nargin < 4) || isempty(T2File)
+        T2File = [];
+    end
     if (nargin < 3) || isempty(isUnload)
         isUnload = 1;
     end
@@ -130,17 +159,51 @@ function sMri = ComputeInteractive(MriFile, Method, isUnload)
             'MNI normalization method', [], {sProcess.options.method.Comment{2,:}, 'Cancel'}, sProcess.options.method.Comment{2,1});
         % Cancel
         if isempty(Method) || strcmpi(Method, 'Cancel')
-            sMri = [];
+            sMriT1 = [];
             return;
+        end
+    end
+    % For Segment method: look for T2 MRI in the subject
+    if strcmpi(Method, 'segment') && isempty(T2File)
+        % Get subject info
+        sSubject = bst_get('MriFile', T1File);
+        % Find any possible T2
+        iT2 = [];
+        if (length(sSubject.Anatomy) > 1)
+            iT2 = find(~cellfun(@(c)isempty(strfind(c,'t2')), lower({sSubject.Anatomy.Comment})));
+            % Warning when multiple T2 images
+            if (length(iT2) > 1)
+                res = java_dialog('question', ...
+                    ['Subject "' sSubject.Name '" has multiple anatomy volumes with tag "T2".' 10 10 ...
+                     'If you want to use a T2 MRI for better volume segmentation, ' 10 ...
+                     'rename or delete the extra T2-labelled files from the subject anatomy.' 10 10 ...
+                     'If you click OK, no T2 image will be used in the segmentation.' 10 10], 'MNI normalization', [], {'OK', 'Cancel'}, 'OK');
+                if isempty(res) || isequal(res, 'Cancel')
+                    return;
+                end
+                iT2 = [];
+            % Confirmation for using T2 image
+            elseif (length(iT2) == 1)
+                isT2 = java_dialog('confirm', ...
+                    ['Subject "' sSubject.Name '" seems to include one T2 MRI, named "' sSubject.Anatomy(iT2).Comment '".' 10 10 ...
+                     'Use this T2 image to refine the volume segmentation?' 10 10], 'MNI normalization');
+                if ~isT2
+                    iT2 = [];
+                end
+            end
+        end
+        % Get T2 filename
+        if ~isempty(iT2)
+            T2File = sSubject.Anatomy(iT2).FileName;
         end
     end
     % Open progress bar
     bst_progress('start', 'MNI normalization', 'Initialization...');
     % Call non-interactive function
-    [sMri, errMsg] = Compute(MriFile, Method);
+    [sMriT1, errMsg] = Compute(T1File, Method, T2File);
     % Error handling
     if ~isempty(errMsg)
-        sMri = [];
+        sMriT1 = [];
         bst_error(errMsg, 'MNI normalization', 0);
     end
     % Close progress bar
