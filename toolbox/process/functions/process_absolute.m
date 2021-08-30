24a760a26e5112a95469564fb929acd4f5057b6a
Raymundo Cassani <raymundo.cassani@gmail.com>
2021-08-30 15:56:05 -0400

    Validate sensor types

--- toolbox/process/functions/process_absolute.m
+++ toolbox/process/functions/process_absolute.m
@@ -61,7 +61,7 @@ function sInput = Run(sProcess, sInput) %#ok<DEFNU>
     % Opposite values
     sInput.A = abs(sInput.A);
     % Warning if applying this process to raw recordings
-    if strcmpi(sInput.FileType, 'raw') &&
+    if strcmpi(sInput.FileType, 'raw') && (isempty(sProcess.options.sensortypes.Value) || any(ismember(lower(strtrim(str_split(sProcess.options.sensortypes.Value, ',;'))), {'eeg', 'meg', 'seeg', 'ecog', 'nirs'})))
         bst_report('Warning', sProcess, sInput, 'Applying an absolute value to raw recordigs is not indicated. Check your processing pipeline.');
     % Change DataType
     elseif ~strcmpi(sInput.FileType, 'timefreq')
