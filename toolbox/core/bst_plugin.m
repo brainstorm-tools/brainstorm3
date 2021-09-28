eea53bdcd343a2b92a41ec989cc067256aa69305
AnneSo <as_dub@hotmail.com>
2021-09-28 11:30:38 +0200

    Plugin MIA  (#442)
    Co-authored-by: Anne-Sophie Dubarry <asdubarry@gmail.com>
    Co-authored-by: Raymundo Cassani <raymundo.cassani@gmail.com>
    Co-authored-by: Francois <francois.tadel@mcgill.ca>

--- toolbox/core/bst_plugin.m
+++ toolbox/core/bst_plugin.m
@@ -353,6 +353,21 @@ function PlugDesc = GetSupported(SelPlug)
     PlugDesc(end).GetVersionFcn  = 'nst_get_version';
     PlugDesc(end).RequiredPlugs  = {'brainentropy'};
     PlugDesc(end).MinMatlabVer   = 803;   % 2014a
+     
+    % === MIA ===
+    PlugDesc(end+1)              = GetStruct('mia');
+    PlugDesc(end).Version        = 'github-master';
+    PlugDesc(end).Category       = 'sEEG';
+    PlugDesc(end).AutoUpdate     = 0;
+    PlugDesc(end).AutoLoad       = 1;
+    PlugDesc(end).CompiledStatus = 2;
+    PlugDesc(end).URLzip         = 'https://github.com/MIA-iEEG/mia/archive/refs/heads/master.zip';
+    PlugDesc(end).URLinfo        = 'http://www.neurotrack.fr/mia/';
+    PlugDesc(end).ReadmeFile     = 'README.md'; 
+    PlugDesc(end).GetVersionFcn  = 'mia_get_version';
+    PlugDesc(end).MinMatlabVer   = 803;   % 2014a
+    PlugDesc(end).LoadFolders    = {'*'};
+    PlugDesc(end).TestFile       = 'process_mia_export_db.m';
     
     % === FIELDTRIP ===
     PlugDesc(end+1)              = GetStruct('fieldtrip');
