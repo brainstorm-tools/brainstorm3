c57e1bb08866d95f23aa6a2be5d70706dcc1bd19
ftadel <francois.tadel@mcgill.ca>
2021-10-12 09:55:38 +0200

    Bugfix: Importing SimNIBS anatomy as the default anatomy

--- toolbox/io/import_anatomy_simnibs.m
+++ toolbox/io/import_anatomy_simnibs.m
@@ -337,7 +337,7 @@ end
 
 %% ===== IMPORT 10-10 POSITIONS =====
 PosFile = bst_fullfile(SimDir, ['m2m_' subjid], 'eeg_positions', 'EEG10-10_UI_Jurak_2007.csv');
-if file_exist(PosFile)
+if file_exist(PosFile) && (iSubject > 0)
     % Create a condition "eeg_positions"
     iStudy = db_add_condition(iSubject, 'eeg_positions');
     % Import channel file
