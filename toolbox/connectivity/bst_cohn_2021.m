820a084c36ddfddd315e1b6e45e60b559cc63989
ftadel <francois.tadel@mcgill.ca>
2021-12-27 10:29:48 +0100

    Distrib: Removed dates from copyright notice (to avoid having to update this every year - authors section is sufficient to indicate the dates)

--- toolbox/connectivity/bst_cohn_2021.m
+++ toolbox/connectivity/bst_cohn_2021.m
@@ -62,7 +62,7 @@ function [Cxy, pValues, freq, nWin, nFFT, Messages] = bst_cohn_2021(Xs, Ys, Fs, 
 % This function is part of the Brainstorm software:
 % https://neuroimage.usc.edu/brainstorm
 % 
-% Copyright (c)2000-2020 University of Southern California & McGill University
+% Copyright (c) University of Southern California & McGill University
 % This software is distributed under the terms of the GNU General Public License
 % as published by the Free Software Foundation. Further details on the GPLv3
 % license can be found at http://www.gnu.org/copyleft/gpl.html.
