04821422a67c1fd2deb702ca5ac0d9846909570c
ftadel <francois.tadel@mcgill.ca>
2022-03-01 10:21:12 +0100

    Bugfix: Added FTP download to bst_websave for Matlab 2021b

--- toolbox/misc/bst_websave.m
+++ toolbox/misc/bst_websave.m
@@ -19,7 +19,37 @@ function errMsg = bst_websave(filename, url)
 % For more information type "brainstorm license" at command prompt.
 % =============================================================================@
 %
-% Authors: Francois Tadel, 2020
+% Authors: Francois Tadel, 2022
+
+errMsg = '';
+
+% FTP download with Matlab >= 2021b
+if (bst_get('MatlabVersion') >= 911) && ~isempty(strfind(url, 'ftp://'))
+    try
+        % Split URL in host/path
+        urlSplit = str_split(url, '/');
+        urlHost = urlSplit{2};
+        if (length(urlSplit) >= 3)
+            urlPath = sprintf('%s/', urlSplit{3:end-1});
+        else
+            urlPath = [];
+        end
+        urlFile = urlSplit{end};
+        % Get destination path
+        [destPath, destFile, destExt] = bst_fileparts(filename);
+        destFile = [destFile, destExt];
+        % Create an FTP object and downloads the file
+        ftpobj = ftp(urlHost);
+        cd(ftpobj, urlPath);
+        mget(ftpobj, urlFile, destPath);
+        close(ftpobj);
+        % Rename downloaded file
+        movefile(bst_fullfile(destPath, urlFile), bst_fullfile(destPath, destFile), 'f');
+        return;
+    catch
+        disp('BST> ERROR: FTP download failed.');
+    end
+end
 
 % Reading function: urlread replaced with webread in Matlab 2014b
 if (bst_get('MatlabVersion') <= 803)
@@ -30,7 +60,6 @@ else
     url_read_alt = @(f,u)urlwrite(u,f);
 end
 % Read online version.txt
-errMsg = '';
 try
     url_read_fcn(filename, url);
 catch
