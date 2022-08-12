145be1c5ba8e039088e92bfcbdcda9812e07509a
ftadel <francois.tadel@mcgill.ca>
2022-08-12 11:15:24 +0200

    out_fs_label: Added copyright

--- toolbox/io/out_label_fs.m
+++ toolbox/io/out_label_fs.m
@@ -7,6 +7,24 @@ function out_label_fs(LabelFile, Comment, Vertices, Pos, Values)
 %    - Vertices  : Vertex indices (0 based, column 1)
 %    - Pos       : Locations in meters (columns 2 - 4 divided by 1000)
 %    - Values    : Values at the vertices (column 5)
+%
+% @=============================================================================
+% This function is part of the Brainstorm software:
+% https://neuroimage.usc.edu/brainstorm
+% 
+% Copyright (c) University of Southern California & McGill University
+% This software is distributed under the terms of the GNU General Public License
+% as published by the Free Software Foundation. Further details on the GPLv3
+% license can be found at http://www.gnu.org/copyleft/gpl.html.
+% 
+% FOR RESEARCH PURPOSES ONLY. THE SOFTWARE IS PROVIDED "AS IS," AND THE
+% UNIVERSITY OF SOUTHERN CALIFORNIA AND ITS COLLABORATORS DO NOT MAKE ANY
+% WARRANTY, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO WARRANTIES OF
+% MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, NOR DO THEY ASSUME ANY
+% LIABILITY OR RESPONSIBILITY FOR THE USE OF THIS SOFTWARE.
+%
+% For more information type "brainstorm license" at command prompt.
+% =============================================================================@
 
 [fid, message] = fopen(LabelFile, 'w');
 if (fid < 0)
