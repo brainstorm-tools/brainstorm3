0c464ae5984918b701023d04e7f89c5113e2d0a2
Marc Lalancette <31040756+Moo-Marc@users.noreply.github.com>
2023-03-28 17:19:02 -0400

    pca: reenable scaling turned off for test

--- toolbox/math/bst_scout_value.m
+++ toolbox/math/bst_scout_value.m
@@ -269,8 +269,7 @@ switch (lower(ScoutFunction))
         PcaFirstComp = bsxfun(@times, CompSign, PcaFirstComp);
         % Rescale before applying component to timeseries. (nComp/nComp is for when we're doing
         % scout and xyz combined, to recover the real number of vertices.)
-        %% TESTING WITHOUT THIS CHANGE vs old code
-        %PcaFirstComp = PcaFirstComp / sqrt(nRow * nComponents / nCompPcaCombined);
+        PcaFirstComp = PcaFirstComp / sqrt(nRow * nComponents / nCompPcaCombined);
         if ~isempty(F)
             Fs = sum(bsxfun(@times, permute(PcaFirstComp, [1,3,2]), F), 1); % dot product of Comp with F on 1st dim, gives size (1, nTime, nComponents)
         else
