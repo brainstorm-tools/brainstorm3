634476f6f16320fffc90fd5798df1da3d2c95982
Edouard <edouard.delaire@icloud.com>
2023-06-21 09:51:29 -0400

    fixed font

--- toolbox/core/bst_get.m
+++ toolbox/core/bst_get.m
@@ -3913,10 +3913,11 @@ switch contextName
         fontTypes = {};
         if (nargin >= 3)
             fontTypes{end + 1} = varargin{3};
+        else
+            fontTypes{end + 1} = 'Arial';  % Default font
+            fontTypes{end + 1} = 'Liberation Sans';  % Free Arial substitute
+            fontTypes{end + 1} = 'Courier New';
         end
-        fontTypes{end + 1} = 'Arial';  % Default font
-        fontTypes{end + 1} = 'Liberation Sans';  % Free Arial substitute
-        
         % Check for cached font
         foundFont = 0;
         for iFont = 1 : length(fontTypes)
