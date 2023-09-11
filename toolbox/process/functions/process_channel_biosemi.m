6576e4caa66c43ca5e07457116508c5af82abd0d
Vatsala Nema <70408681+NemaVatsala@users.noreply.github.com>
2023-09-11 20:32:26 +0530

    Update process_channel_biosemi.m

--- toolbox/process/functions/process_channel_biosemi.m
+++ toolbox/process/functions/process_channel_biosemi.m
@@ -35,7 +35,7 @@ function sProcess = GetDescription()
     sProcess.Category    = 'Custom';
     sProcess.SubGroup    = {'Import', 'Channel file'};
     sProcess.Index       = 43;
-    sProcess.Description = '';
+    sProcess.Description = 'Renames Biosemi caps labels to the 10-10 EEG Standard';
     sProcess.isSeparator = 1;
     % Definition of the input accepted by this process
     sProcess.InputTypes  = {'data', 'raw'};
@@ -68,29 +68,47 @@ function OutputFiles = Run(sProcess, sInputs)
     %       - There are EEG channels, AND
     %       -Find indices of channels with type 'EEG'
     eegIndices = find(strcmpi({ChannelMat.Channel.Type}, 'EEG'));
+      
 
     if isempty(eegIndices)
         bst_report('Error', sProcess, [], 'No EEG Channel was taken into account');
     end
-     
+        
+        % Use the indices to filter the channels
+        eegChannels = ChannelMat.Channel(eegIndices);
+        [m, n] = size(eegChannels);
     
-    % Use the indices to filter the channels
-    eegChannels = ChannelMat.Channel(eegIndices);
-    
+        % Initialize a cell array
+        eegarray = cell(m, n);
+        eegcarray=cell(m,n);
+        
         %       - Current channels names correspond to Biosemi style: A1...A32, B1...B32 C1... and so on, AND
         %         (Be aware of multiple naming e.g., A1 == A01 === A001, ...)
         %        - Countering/removing the additional zeros from the
         %        channel name using regex 
 
-        for i = 1:length(eegChannels)
-            newValue = regexprep(value, '0(?=[1-9])', '');
-            eegChannels(i) = newValue;   
+        % Remove leading zeros before digits 1-9
+        for i = 1:length(eegarray)
+            channel = eegarray{i};
+            % Find the first non-zero digit
+            firstNonZero = find(channel ~= '0', 1);
+            if ~isempty(firstNonZero)
+                % Remove leading zeros
+                eegcarray{i} = channel(firstNonZero:end);
+            end
         end
+            eegChannels=eegcarray;
         %        AND
         %       - All EEG channels make up a Biosemi cap, no 1 channel more, no 1 channel less.
-        ChannelMaps=GetBiosemiMapping(eegChannels);
-        missingEEGElectrodes = setdiff(keys(ChannelMaps), eegchannels);
-        extraEEGElectrodes = setdiff(eegchannels, keys(ChannelMap));
+        
+        ChannelMaps=GetBiosemiMaps(eegChannels);
+        %ChannelMapcarray=cellfun(keys(ChannelMaps));
+
+        keysArray = keys(ChannelMaps);
+        keysCellArray = cellstr(keysArray);
+
+        missingEEGElectrodes = setdiff(keysCellArray, eegcarray);
+        extraEEGElectrodes = setdiff(eegcarray, keysCellArray);
 
         %       - Otherwise return without changes
 
@@ -183,7 +201,7 @@ end
 %% ===== NAME MAPS =====
 function ChannelMap = GetBiosemiMaps(eegchannels)
 %Biosemi structure can be found at: https://www.biosemi.com/headcap.htm
-    %ChannelNameMaps = [];
+    ChannelMap = containers.Map;
         mapping64 = containers.Map({'A1', 'B1', 'B2', 'A2', 'A3', 'B5', 'B4', 'B3', 'A7', 'A6', 'A5', 'A4', 'B6', 'B7', ...
             'B8', 'B9', 'B10', 'A8', 'A9', 'A10', 'A11', 'B15', 'B14', 'B13', 'B12', 'B11', 'A15', 'A14', 'A13', 'A12', ...
             'B16', 'B17', 'B18', 'B19', 'B20', 'A16', 'A17', 'A18', 'A19', 'A32', 'B24', 'B23', 'B22', 'B21', 'A24', ...
@@ -191,12 +209,12 @@ function ChannelMap = GetBiosemiMaps(eegchannels)
             'A27', 'A29', 'B32', 'A28'}, {'Fp1', 'Fpz', 'Fp2', 'AF7', 'AF3', 'AFz', 'AF4', 'AF8', 'F7', 'F5', 'F3', ...
             'F1', 'Fz', 'F2', 'F4', 'F6', 'F8', 'FT7', 'FC5', 'FC3', 'FC1', 'FCz', 'FC2', 'FC4', 'FC6', 'FT8', 'T7', ...
             'C5', 'C3', 'C1', 'Cz', 'C2', 'C4', 'C6', 'T8', 'TP7', 'CP5', 'CP3', 'CP1', 'CPz', 'CP2', 'CP4', 'CP6', ...
-            'TP8', 'P9', 'P7', 'P5', 'P3', 'P1', 'Pz', 'P2', 'P4', 'P6', 'P8', 'P10', 'PO7', 'PO3', 'POZ', 'PO4', ...
+            'TP8', 'P9', 'P7', 'P5', 'P3', 'P1', 'Pz', 'P2', 'P4', 'P6', 'P8', 'P10', 'PO7', 'PO3', 'POZ', 'PO4', 'PO8'...
             'O1', 'OZ', 'O2', 'IZ'});
 
         mapping32 = containers.Map({'A1', 'A30','A2','A29', 'A3','A4' 'A31', 'A27', 'A28', 'A6','A5','A26', 'A25','A7','A8', ...
             'A32', 'A23', 'A24', 'A10', 'A9','A22', 'A21','A11','A12', 'A13','A19', 'A20', 'A14','A18','A15', 'A16','A17'}, ...
-            {'Fp1','Fp2', 'AF3','AF4', 'F7','F3', 'Fz','F4', 'F8', 'FC5','FC1','FC2','FC6', 'C3', 'Cz', 'C4', 'T8', 'CP5', ...
+            {'Fp1','Fp2', 'AF3','AF4', 'F7','F3', 'Fz','F4', 'F8', 'FC5','FC1','FC2','FC6', 'T7', 'C3', 'Cz', 'C4', 'T8', 'CP5', ...
             'CP1','CP2','CP6','P7','P3','Pz','P4' 'P8', 'PO3','PO4','O1', 'Oz','O2'});
       
     % TODO: Generate one-to-one channel name maps for Biosemi caps
@@ -205,12 +223,10 @@ function ChannelMap = GetBiosemiMaps(eegchannels)
     %       Or they can be containers.Map objects- more flexible
 
     % Create a dictionary-like structure for mapping
-    numEEGChannels=sum(eegchannels);
+    numEEGChannels=length(eegchannels);
        if numEEGChannels == 32
            ChannelMap = mapping32;        
        elseif numEEGChannels == 64
            ChannelMap = mapping64;   
-       else
-          bst_report('Error', sProcess, [], 'The number of channels is not 32 or 64. Please check the channel file.');
        end       
 end
