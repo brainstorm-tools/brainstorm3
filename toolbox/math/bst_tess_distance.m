bdb4ebd98ff7aa20184cb12a1e59dfab00d08e45
Edouard <edouard.delaire@icloud.com>
2023-09-23 17:35:05 -0400

    tess_distance

---
+++ toolbox/math/bst_tess_distance.m
@@ -0,0 +1,59 @@
+function Dist = bst_tess_distance(SurfaceMat, VerticesA, VerticesB, metric)
+% bst_tess_distance: For every vertex B, return the minimum distance to the vertices A using
+% either the euclidean metric (metric = euclidean), or geodesic mesuring the distance either as
+% number of edge (metric = geodesic_edge) or path lenght (geodesic_length)
+% When using the geodesic distance, if two nodes are not
+% connected, the distance is infinite. 
+% @=============================================================================
+% This function is part of the Brainstorm software:
+% https://neuroimage.usc.edu/brainstorm
+% 
+% Copyright (c) University of Southern California & McGill University
+% This software is distributed under the terms of the GNU General Public License
+% as published by the Free Software Foundation. Further details on the GPLv3
+% license can be found at http://www.gnu.org/copyleft/gpl.html.
+% 
+% FOR RESEARCH PURPOSES ONLY. THE SOFTWARE IS PROVIDED "AS IS," AND THE
+% UNIVERSITY OF SOUTHERN CALIFORNIA AND ITS COLLABORATORS DO NOT MAKE ANY
+% WARRANTY, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO WARRANTIES OF
+% MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, NOR DO THEY ASSUME ANY
+% LIABILITY OR RESPONSIBILITY FOR THE USE OF THIS SOFTWARE.
+%
+% For more information type "brainstorm license" at command prompt.
+% =============================================================================@
+%
+% Authors: Edouard Delaire 2023
+
+    Dist = zeros(length(VerticesB),1); 
+    Vertices = SurfaceMat.Vertices;
+    if strcmp(metric,'euclidean')
+        y = Vertices(VerticesA,:)'*1000; 
+        for i = 1:length(VerticesB)
+            x = Vertices(VerticesB(i),:)'*1000; 
+            Dist(i)=min( sum((y-x).^2).^0.5); %euclidean distance calculation
+        end
+
+    elseif contains(metric,'geodesic') 
+
+        D = zeros(size(SurfaceMat.VertConn));
+        [vi,vj] = find(SurfaceMat.VertConn);
+
+        if strcmp(metric,'geodesic_length')
+            for i=1:size(vi, 1)
+                x =  Vertices(vi(i),:)' * 1000;
+                y =  Vertices(vj(i),:)' * 1000;
+
+                D(vi(i), vj(i)) =  sum((y-x).^2).^0.5;
+            end
+        else
+            D = full(SurfaceMat.VertConn);
+        end
+
+        G = digraph(D);
+
+        for i =1:length(VerticesB)
+            Dist(i)=min( distances(G, VerticesA,VerticesB(i)));
+        end
+
+    end 
+end
