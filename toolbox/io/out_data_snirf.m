de1aa51df0681b192f208370c0bf62c468f3cb68
Edouard <edouard.delaire@icloud.com>
2023-10-22 18:22:44 -0400

    Improve file export
    generated snirf file can now be open in AnalyzIR toolbox. Remaining bug: doesnt work if there is no event or empty events

--- toolbox/io/out_data_snirf.m
+++ toolbox/io/out_data_snirf.m
@@ -50,14 +50,6 @@ for i_aux=1:n_aux
     snirfdata.SNIRFData.aux(i_aux).timeOffset = 0;
 end    
 
-% Set Probe; maybe can be simplified with the export of the measurment list
-[isrcs, idets, chan_measures, measure_type] = nst_unformat_channels({ChannelMatOut.Channel(nirs_channels).Name});
-
-src_pos= zeros(length(unique(isrcs)),3); 
-src_label = cell(1,length(unique(isrcs)));
-det_pos= zeros(length(unique(idets)),3); 
-det_label= cell(1,length(unique(idets)));
-
 % Set landmark position (eg fiducials) 
 n_landmark=length(ChannelMatOut.HeadPoints.Label);
 snirfdata.SNIRFData.probe.landmarkPos=zeros(n_landmark,3);
@@ -66,20 +58,49 @@ for i_landmark=1:n_landmark
     snirfdata.SNIRFData.probe.landmarkLabels(i_landmark)=string(ChannelMatOut.HeadPoints.Label{i_landmark}); 
 end    
 
+% Set Probe; maybe can be simplified with the export of the measurment list
+[isrcs, idets, chan_measures, measure_type] = nst_unformat_channels({ChannelMatOut.Channel(nirs_channels).Name});
+
+src_pos     = zeros(length(unique(isrcs)),3); 
+src_label   = repmat( {''}, 1,length(unique(isrcs)));
+src_Index   = zeros(length(unique(isrcs)),1);
+det_pos     = zeros(length(unique(idets)),3); 
+det_label   = repmat( {''}, 1,length(unique(idets)));
+det_Index   = zeros(length(unique(isrcs)),1);
+
+% Set Measurment list
+nSrc = 1;
+nDet = 1;
+for ichan=1:n_channel
+    [isrc, idet, chan_measures, measure_type] = nst_unformat_channels({ChannelMatOut.Channel(ichan).Name});
+
+    if ~any(cellfun(@(x)strcmp(x, sprintf('S%d',isrc )), src_label))
+        src_label{nSrc} = sprintf('S%d',isrc );
+        src_Index(nSrc) = isrc;
+        src_pos(nSrc,:)=ChannelMatOut.Channel(ichan).Loc(:,1)';
+
+        nSrc = nSrc + 1;
+    end
+
+    if ~any(cellfun(@(x)strcmp(x, sprintf('D%d',idet )), det_label))
+        det_label{nDet} = sprintf('D%d',idet );
+        det_Index(nDet) = idet;
+        det_pos(nDet,:)=ChannelMatOut.Channel(ichan).Loc(:,1)';
+
+        nDet = nDet + 1;
+    end
+
+end
+
 % Set Measurment list
 for ichan=1:n_channel
     measurement=struct('sourceIndex',[],'detectorIndex',[],...
               'wavelengthIndex',[],'dataType',1,'dataTypeIndex',1); 
     [isrc, idet, chan_measures, measure_type] = nst_unformat_channels({ChannelMatOut.Channel(ichan).Name});
 
-    src_pos(isrc,:)=ChannelMatOut.Channel(ichan).Loc(:,1)';
-    src_label{isrc} = sprintf('S%d',isrc);
-    det_pos(idet,:)=ChannelMatOut.Channel(ichan).Loc(:,2)';
-    det_label{idet} = sprintf('D%d',idet);
-
-    measurement.sourceIndex=isrc;
-    measurement.detectorIndex=idet;
-    measurement.wavelengthIndex=find(ChannelMatOut.Nirs.Wavelengths==chan_measures);
+    measurement.sourceIndex     = find(src_Index == isrc);
+    measurement.detectorIndex   = find(det_Index == idet);
+    measurement.wavelengthIndex = find(ChannelMatOut.Nirs.Wavelengths==chan_measures);
 
     snirfdata.SNIRFData.data.measurementList(ichan)=measurement;      
 
@@ -87,13 +108,11 @@ end
 
 snirfdata.SNIRFData.probe.wavelengths=ChannelMatOut.Nirs.Wavelengths;
 
-snirfdata.SNIRFData.probe.sourcePos=src_pos;
-snirfdata.SNIRFData.probe.sourcePos(:,3)=0; % set z to 0
+snirfdata.SNIRFData.probe.sourcePos2D=src_pos(:,[1,2]);
 snirfdata.SNIRFData.probe.sourcePos3D=src_pos;
 snirfdata.SNIRFData.probe.sourceLabels = src_label';
 
-snirfdata.SNIRFData.probe.detectorPos=det_pos; 
-snirfdata.SNIRFData.probe.detectorPos(:,3)=0; % set z to 0 
+snirfdata.SNIRFData.probe.detectorPos2D=det_pos(:,[1,2]);
 snirfdata.SNIRFData.probe.detectorPos3D=det_pos;
 snirfdata.SNIRFData.probe.detectorLabels=det_label';
 
