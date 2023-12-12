brainstorm-tools/brainstorm3

commit f30c9051c6665205f141b5e90e0404d656bc35f5

Brainstorm: Apple silicon support (#663)
* add ARM version of SPM for arm mac

* apply change

* Add `maca` binaries in `external/spm/` (for maff MNI normalization)

* Add warning about Apple silicon

* Add error for plugins not supported in Apple silicon

* add error when loading

---------

Co-authored-by: rcassani <raymundo.cassani@gmail.com>

toolbox/core/bst_plugin.m
@@ -628,7 +628,14 @@ function PlugDesc = GetSupported(SelPlug)
     PlugDesc(end+1)              = GetStruct('spm12');
     PlugDesc(end).Version        = 'latest';
     PlugDesc(end).AutoUpdate     = 0;
-    PlugDesc(end).URLzip         = 'https://www.fil.ion.ucl.ac.uk/spm/download/restricted/eldorado/spm12.zip';
+    switch(OsType)
+        case  'mac64arm'
+            PlugDesc(end).URLzip         = 'https://github.com/spm/spm12/archive/refs/heads/maint.zip';
+            PlugDesc(end).Version        = 'github-maint';
+        otherwise
+            PlugDesc(end).Version        = 'latest';
+            PlugDesc(end).URLzip         = 'https://www.fil.ion.ucl.ac.uk/spm/download/restricted/eldorado/spm12.zip';
+    end
     PlugDesc(end).URLinfo        = 'https://www.fil.ion.ucl.ac.uk/spm/';
     PlugDesc(end).TestFile       = 'spm.m';
     PlugDesc(end).ReadmeFile     = 'README.md';
@@ -1258,9 +1265,16 @@ function [isOk, errMsg, PlugDesc] = Install(PlugName, isInteractive, minVersion)
     if ~isempty(errMsg)
         return;
     end
+    % Check if plugin is supported on Apple silicon
+    OsType = bst_get('OsType', 0);
+    if strcmpi(OsType, 'mac64arm') && ismember(PlugName, PluginsNotSupportAppleSilicon())
+        errMsg = ['Plugin ', PlugName ' is not supported on Apple silicon yet.'];
+        PlugDesc = [];
+        return;
+    end
     % Check if there is a URL to download
     if isempty(PlugDesc.URLzip)
-        errMsg = ['No download URL for ', bst_get('OsType', 0), ': ', PlugName ''];
+        errMsg = ['No download URL for ', OsType, ': ', PlugName ''];
         return;
     end
     % Compiled version
@@ -1855,6 +1869,12 @@ function [isOk, errMsg, PlugDesc] = Load(PlugDesc, isVerbose)
     % Get plugin structure from name
     [PlugDesc, errMsg] = GetDescription(PlugDesc);
     if ~isempty(errMsg)
+        return;
+    end
+    % Check if plugin is supported on Apple silicon
+    OsType = bst_get('OsType', 0);
+    if strcmpi(OsType, 'mac64arm') && ismember(PlugName, PluginsNotSupportAppleSilicon())
+        errMsg = ['Plugin ', PlugDesc.Name ' is not supported on Apple silicon yet.'];
         return;
     end
     % Minimum Matlab version
@@ -2831,3 +2851,10 @@ function SetProgressLogo(PlugDesc)
     end
 end
 
+
+%% ===== NOT SUPPORTED APPLE SILICON =====
+% Return list of plugins not supported on Apple silicon
+function pluginNames = PluginsNotSupportAppleSilicon()
+    pluginNames = {'brain2mesh', 'ct2mrireg', 'duneuro', 'iso2mesh', 'mcxlab-cl', 'mcxlab-cuda', ...
+                   'openmeeg', 'xdf'};
+end

toolbox/core/bst_startup.m
@@ -148,6 +148,12 @@ if (MatlabVersion <= 803)
     disp('BST> Warning: For better graphics, use Matlab >= 2014b');
 end
 
+% Check for Apple silicon (started with R2023b)
+if (MatlabVersion >= 2302) && strcmp(bst_get('OsType', 0), 'mac64arm')
+    disp(['BST> Warning: Running on Apple silicon, some functions and plugins are not supported yet:' 10 ...
+          '              Use Matlab < 2023b or Matlab for Intel processor for full support']);
+end
+
 
 %% ===== FORCE COMPILATION OF SOME INTERFACE FILES =====
 if (GuiLevel == 1)

commit 9aa2bda568c3949f2ffce30700ac7108d31aac6f

Bugfix: Bug introduced in (#663)

doc/license.html
@@ -5,7 +5,7 @@
 <body alink="#fff000" link="#fff000" vlink="#fff000">
     <h4><span style="font-family: Arial Black; color: #ffffff;"><strong>THERE IS NO UNDO BUTTON!<BR>SET UP A <FONT color=red>BACKUP</FONT> OF YOUR DATABASE</strong></span></h4>
 <HR>
-<!-- LICENCE_START -->Version: 3.231211 (11-Dec-2023)<br>
+<!-- LICENCE_START -->Version: 3.231212 (12-Dec-2023)<br>
 <span style="font-style: italic;">COPYRIGHT &copy; 2000-2023
 USC &amp; McGill University.<br>
 </span>

toolbox/core/bst_plugin.m
@@ -1873,7 +1873,7 @@ function [isOk, errMsg, PlugDesc] = Load(PlugDesc, isVerbose)
     end
     % Check if plugin is supported on Apple silicon
     OsType = bst_get('OsType', 0);
-    if strcmpi(OsType, 'mac64arm') && ismember(PlugName, PluginsNotSupportAppleSilicon())
+    if strcmpi(OsType, 'mac64arm') && ismember(PlugDesc.Name, PluginsNotSupportAppleSilicon())
         errMsg = ['Plugin ', PlugDesc.Name ' is not supported on Apple silicon yet.'];
         return;
     end