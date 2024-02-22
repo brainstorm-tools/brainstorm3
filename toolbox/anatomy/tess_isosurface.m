786a57340dfe225ec13dba84f6c053fa02491505
Chinmay Chinara <chinmay.chinara@gmail.com>
2024-02-21 17:01:13 -0800

    if user cancels generation of isosurface

--- toolbox/anatomy/tess_isosurface.m
+++ toolbox/anatomy/tess_isosurface.m
@@ -64,9 +64,9 @@ if isempty(sMri)
     % Load MRI
     bst_progress('start', 'Generate thresholded isosurface from CT', 'Loading CT...');
     sMri = bst_memory('LoadMri', MriFile);
-    % if isProgress
-    %     bst_progress('stop');
-    % end
+    if isProgress
+        bst_progress('stop');
+    end
 end
 % Save current scouts modifications
 panel_scout('SaveModifications');
