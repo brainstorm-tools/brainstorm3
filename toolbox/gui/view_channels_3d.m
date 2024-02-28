035030cb007263689261facbfbe7bc36cf9edafa
Chinmay Chinara <chinmay.chinara@gmail.com>
2024-02-28 14:40:44 -0800

    renaming back to 'hFig' as it has dependency below

--- toolbox/gui/view_channels_3d.m
+++ toolbox/gui/view_channels_3d.m
@@ -115,14 +115,14 @@ if ~isempty(sSubject)
                     SurfaceFile = sSubject.Anatomy(sSubject.iAnatomy).FileName;
                 end
                 SurfAlpha = .1;
-                
+
                 % Get current 3D figure
-                hFig3d = bst_figures('GetFiguresByType', {'3DViz'});
-                if isempty(hFig3d)
-                    hFig3d = view_mri_3d(SurfaceFile, [], SurfAlpha, 'NewFigure');
+                hFig = bst_figures('GetFiguresByType', {'3DViz'});
+                if isempty(hFig)
+                    hFig = view_mri_3d(SurfaceFile, [], SurfAlpha, 'NewFigure');
                 end
                 % display isosurface
-                panel_ieeg('DisplayIsosurface', sSubject, hFig3d);
+                panel_ieeg('DisplayIsosurface', sSubject, hFig);
             end
     end
 end
