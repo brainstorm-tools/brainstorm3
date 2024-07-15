fb0b5fa7cfff4c71b6024c8e4ade3b7c440a716a
rcassani <raymundo.cassani@gmail.com>
2024-07-15 16:34:10 -0400

    Add license header

--- toolbox/process/functions/process_mtrf_train.m
+++ toolbox/process/functions/process_mtrf_train.m
@@ -1,4 +1,27 @@
 function varargout = process_mtrf_train( varargin )
+% process_mtrf_train: Fits an encoding/decoding model using mTRF-Toolbox
+
+% @=============================================================================
+% This function is part of the Brainstorm software:
+% https://neuroimage.usc.edu/brainstorm
+% 
+% Copyright (c) University of Southern California & McGill University
+% This software is distributed under the terms of the GNU General Public License
+% as published by the Free Software Foundation. Further details on the GPLv3
+% license can be found at http://www.gnu.org/copyleft/gpl.html.
+% 
+% FOR RESEARCH PURPOSES ONLY. THE SOFTWARE IS PROVIDED "AS IS," AND THE
+% UNIVERSITY OF SOUTHERN CALIFORNIA AND ITS COLLABORATORS DO NOT MAKE ANY
+% WARRANTY, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO WARRANTIES OF
+% MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, NOR DO THEY ASSUME ANY
+% LIABILITY OR RESPONSIBILITY FOR THE USE OF THIS SOFTWARE.
+%
+% For more information type "brainstorm license" at command prompt.
+% =============================================================================@
+%
+% Authors: Anna Zaidi, 2024
+%          Raymundo Cassani, 2024
+
 eval(macro_method);
 end
 
