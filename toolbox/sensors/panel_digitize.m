85834a63c81d3d6449fb7ceb52975d632cdf3309
Chinmay Chinara <chinmay.chinara@gmail.com>
2024-09-24 14:56:42 -0700

    bugfix

--- toolbox/sensors/panel_digitize.m
+++ toolbox/sensors/panel_digitize.m
@@ -212,6 +212,7 @@ function Start(varargin) %#ok<DEFNU>
                     % If more than one surface present, user can choose
                     if length(iSurface) > 1
                         texSurfComment = java_dialog('combo', '<HTML>Select the textured surface:<BR><BR>', 'Choose textured surface', [], {sSubject.Surface(iSurface).Comment});
+                        texSurfComment = erase(texSurfComment, '_defaced');
                         if isempty(texSurfComment)
                             return
                         end
