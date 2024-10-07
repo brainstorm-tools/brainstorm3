b2b67091d5bce84a3a2f9f387a806e59f7ba3abb
Edouard <edouard.delaire@icloud.com>
2024-10-07 17:16:53 -0400

    FIX

--- toolbox/process/functions/process_inverse.m
+++ toolbox/process/functions/process_inverse.m
@@ -697,8 +697,7 @@ function [OutputFiles, errMessage] = Compute(iStudies, iDatas, OPTIONS)
                 if ~isfield(Results, 'nComponents') || isempty(Results.nComponents)
                     Results.nComponents = round(max(size(Results.ImageGridAmp,1),size(Results.ImagingKernel,1)) / nSources);
                 end
-
-                Results.nComponents = round(max(size(Results.ImageGridAmp,1),size(Results.ImagingKernel,1)) / nSources);
+                
                 % Get outputs
                 DataFile = OPTIONS.DataFile; 
                 Time     = OPTIONS.DataTime;
