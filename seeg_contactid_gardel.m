34867c262caf9a1c5bf5899b59dd29192aafa5b1
Chinmay Chinara <chinmay.chinara@gmail.com>
2024-10-15 17:47:53 -0700

    test1: import GARDEL data back into BST

--- seeg_contactid_gardel.m
+++ seeg_contactid_gardel.m
@@ -104,14 +104,16 @@ end
 jBstFrame = bst_get('BstFrame');
 jBstFrame.setVisible(0);
 
-% call the external GARDEL tool
+% Call the external GARDEL tool
 bst_call(@GARDEL);
+% bst_call(@GARDEL,'output_dir',TmpGardelDir, ...
+%     'postimp',NiiRawCtFile, 'preimp',NiiRefMriFile);
 
 % Set process logo
 bst_progress('stop');
 
-% find the app 'GARDEL_v2.3.7'
-% TODO: Save data to temporary folder from GARDEL
+% Find the app 'GARDEL_v2.3.7'
+% Save data to temporary folder from GARDEL
 appName = 'GARDEL_v2.3.7';
 disp([appName ' app opened !']);
 f = findall(bst_get('groot'),'Type','figure','Name',appName);
@@ -119,5 +121,92 @@ waitfor(f);
 disp([appName ' app closed !']);
 
 % Show Brainstorm GUI
-% TODO: Load data from temp directory back to Brainstorm 
 jBstFrame.setVisible(1);
+
+% TODO: Load data from temp directory back to Brainstorm 
+%% Check if electrode coordinates txt file was exported 
+electrodes_file_path = [TmpGardelDir '\ElectrodesAllCoordinates.txt'];
+if ~exist(electrodes_file_path, 'file')
+    bst_error('Electrode coordinates file not found. Make sure you export before quitting GARDEL !', 'GARDEL', 0);
+    % comment this line to keep the temporary folder
+    file_delete(TmpGardelDir, 1, 1);
+    return
+end
+
+%% Make a new empty channel file for the subject
+% Get subject
+sSubject = bst_get('Subject', iSubject);
+SubjectName = sSubject.Name;
+sMri = bst_memory('LoadMri', sSubject.Anatomy(1).FileName);
+
+% Get current date/time
+c = clock;
+% Create a unique channel file each time we try running GARDEL from BST (may not be required in future) 
+for i = 1:99
+    % Generate new condition name
+    ConditionName = sprintf('%s_%02d%02d%02d_%02d', SubjectName, c(1), c(2), c(3), i);
+    % Get condition
+    [sStudy, ~] = bst_get('StudyWithCondition', [SubjectName '/' ConditionName]);
+    % If condition doesn't exist: ok, keep this one
+    if isempty(sStudy)
+        break;
+    end
+end
+% Create condition
+iStudy = db_add_condition(SubjectName, ConditionName);
+sStudy = bst_get('Study', iStudy);
+
+%% Create an empty channel file in GARDEL
+ChannelMat = db_template('channelmat');
+ChannelMat.Channel = db_template('channeldesc');
+ChannelMat.Comment = ConditionName;
+
+%% Parse the electrode coordinates txt file and load it to the channel file
+fid = fopen(electrodes_file_path);
+tline = fgets(fid);
+while isempty(strfind(tline,'MRI_voxel'))
+    tline = fgets(fid);
+end
+tline = fgets(fid);
+Electrodes = [];
+i = 1;
+while ischar(tline) && ~contains(tline,'MRI_FS')
+    if isempty(strfind(tline,'#'))
+        Electrodes = [Electrodes; textscan(tline, '%s %f %f %f %f %f %s %s', 8, 'Delimiter', '\t')];
+        i = i+1;
+    end
+    tline = fgets(fid);
+end
+
+% Parse the 'Electrodes' variable and put it in the BST format in the channel file
+for ii=1:length(Electrodes)
+    a = Electrodes(ii, 1);
+    b = Electrodes(ii, 2);
+    ChannelMat.Channel(ii).Name = [a{:}{:} num2str(b{:})];
+
+    ChannelMat.Channel(ii).Group = a{:}{:};
+    
+    x = Electrodes(ii, 3);
+    y = Electrodes(ii, 4);
+    z = Electrodes(ii, 5);
+    xx(1) = x{:};
+    xx(2) = y{:};
+    xx(3) = z{:};
+
+    % Convert from GARDEL MRI Voxel space to BST SCS coordinates
+    xx = cs_convert(sMri, 'voxel', 'scs', xx);
+    ChannelMat.Channel(ii).Loc = xx';
+
+    ChannelMat.Channel(ii).Type = 'SEEG';
+end
+
+%% Save the new channel file
+ChannelFile = bst_fullfile(bst_fileparts(file_fullpath(sStudy.FileName)), ['channel_' ConditionName '.mat']);
+save(ChannelFile, '-struct', 'ChannelMat');
+
+% Reload condition
+db_reload_studies(iStudy);
+
+% delete temporary folder
+% comment this line to keep the temporary folder
+% file_delete(TmpGardelDir, 1, 1);
