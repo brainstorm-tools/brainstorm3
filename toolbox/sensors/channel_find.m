fcba997b6fe1b595246d460908a8f47b10c40e73
Edouard <edouard.delaire@icloud.com>
2025-01-07 10:36:42 -0500

    fix bug on channel_find

--- toolbox/sensors/channel_find.m
+++ toolbox/sensors/channel_find.m
@@ -61,13 +61,13 @@ for i = 1:length(target)
     else
         iChan = find(strcmpi(allNames, target{i}));
         % Search for NIRS channels using Source, Detector or Wavelength
-        if ismember('NIRS', allTypes)
+        if isempty(iChan) && ismember('NIRS', allTypes)
             % Find tokens in provided target
             % Token 1: Source,               e.g. 'S1'
             % Token 2: Detector,             e.g. 'D1'
             % Token 1: Wavelength or Metric, e.g. 'WL830'
             target_tokens = regexp(target{i}, '^(S([0-9]+)?)?(D([0-9]+)?)?(WL\d+|HbO|HbR|HbT)?$', 'tokens');
-            if isempty(target_tokens)
+            if isempty(target_tokens) 
                 continue;
             end 
             target_tokens = target_tokens{1};
