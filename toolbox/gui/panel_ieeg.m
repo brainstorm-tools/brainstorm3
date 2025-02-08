e76b951daddab8ea616eedfad009fa3f0288fe9e
Chinmay Chinara <chinmay.chinara@gmail.com>
2025-02-07 16:58:23 -0800

    Remove code repetition: Function to retrieve CT volume index and isoValue from the IsoSurface data
    Some comments cleanup as well

--- toolbox/gui/panel_ieeg.m
+++ toolbox/gui/panel_ieeg.m
@@ -418,49 +418,34 @@ function AutoElecLabelContLocalize(Method)
     switch(lower(Method))
         case 'gardel'
             disp('Processing using GARDEL.');
-            CtFile   = [];
-            isoValue = [];
-            iCtVol   = find(cellfun(@(x) ~isempty(regexp(x, '_volct', 'match')), {sSubject.Anatomy.FileName}));
             iIsoSrf  = find(cellfun(@(x) ~isempty(regexp(x, '_isosurface', 'match')), {sSubject.Surface.FileName}));
-            % Get CT from IsoSurf ans also the isoValue % TODO do not assume there is only one IsoSurf
-            if ~isempty(iIsoSrf)
-                sSurf = load(file_fullpath(sSubject.Surface(iIsoSrf).FileName), 'History');
-                if isfield(sSurf, 'History') && ~isempty(sSurf.History)
-                    % Search for CT threshold in history
-                    ctEntry  = regexp(sSurf.History{1, 3}, '^Thresholded CT:\s(.*)\sthreshold.*$', 'tokens', 'once');
-                    isoValue = regexp(sSurf.History{1, 3}, 'threshold\s*=\s*(\d+)', 'tokens', 'once');
-                    % Return intersection of the found and then update iCtVol
-                    if ~isempty(ctEntry)
-                        [~, iCtIso] = ismember(ctEntry{1}, {sSubject.Anatomy.FileName});
-                        if iCtIso
-                            iCtVol = intersect(iCtIso, iCtVol);
-                        else
-                            bst_error(sprintf(['The CT that was used to create the IsoSurface cannot be found. ' 10 ...
-                                               'CT file : %s'], ctEntry{1}), 'Loading CT for IsoSurface');
-                            return
-                        end
-                    end
-                end
-                CtFile = sSubject.Anatomy(iCtVol).FileName;
-            else
-                bst_error('No IsoSurface available.', 'Loading IsoSurface', 0);
-                return
+            % Check if any IsoSurface is available
+            if isempty(iIsoSrf)
+                bst_error('No IsoSurface available.', 'Loading IsoSurface');
+                return;
             end
-            % Load CT file
-            sCt = bst_memory('LoadMri', CtFile);
-            % Set voxel size as per GARDEL input requirements
-            CT_info.pixdim = sCt.Voxsize;
-            
+            % Retrieve CT volume index and isoValue from the IsoSurface data
+            [iCtVol, isoValue] = GetIsosurfaceData(sSubject, iIsoSrf);
+            % Ensure isoValue is available; otherwise, exit with an error
+            if isempty(isoValue)
+                bst_error('No isoValue available. Cannot proceed as GARDEL requires it for electrode segmentation.', 'GARDEL: Automatic electrode labeling and contact localization');
+                return;
+            end     
+            sCt = [];
+            % If a CT volume index was returned, load CT file
+            if ~isempty(iCtVol)
+                sCt = bst_memory('LoadMri', sSubject.Anatomy(iCtVol).FileName);
+                % Set voxel size as per GARDEL input requirements
+                infoCt.pixdim = sCt.Voxsize;
+            end             
             % Set process logo
             bst_progress('start', 'GARDEL', 'Detecting electrodes and contacts...');
-            bst_plugin('SetProgressLogo', 'gardel');
-            
+            bst_plugin('SetProgressLogo', 'gardel');           
             % Use GARDEL automatic segmentation of electrodes            
-            sElectrodes = elec_auto_segmentation(sCt.Cube, CT_info, str2double(isoValue{1}));
-
+            sElectrodes = elec_auto_segmentation(sCt.Cube, infoCt, isoValue);
             % Based on the number of electrodes detected, generate a list of electrode labels recursively
             electrodeLabels = GenerateElecLabels(size(sElectrodes, 1));
-            
+           
             % Loop through detected electrodes
             for iElec = 1:size(sElectrodes, 1)
                 % Extract contacts for current electrode
@@ -481,7 +466,7 @@ function AutoElecLabelContLocalize(Method)
             end
 
         otherwise
-            bst_error(['Invalid method: ' Method], 'Automatic electrode labeling and contact localization', 0);
+            bst_error(['Invalid method: ' Method], 'Automatic electrode labeling and contact localization');
             return
     end
 
@@ -3284,22 +3269,7 @@ function CreateImplantation(MriFile) %#ok<DEFNU>
         end
         % Get CT from IsoSurf  % TODO do not assume there is only one IsoSurf
         if ~isempty(iSrf)
-            sSurf = load(file_fullpath(sSubject.Surface(iSrf).FileName), 'History');
-            if isfield(sSurf, 'History') && ~isempty(sSurf.History)
-                % Search for CT threshold in history
-                ctEntry = regexp(sSurf.History{1, 3}, '^Thresholded CT:\s(.*)\sthreshold.*$', 'tokens', 'once');
-                % Return intersection of the found and then update iCtVol
-                if ~isempty(ctEntry)
-                    [~, iCtIso] = ismember(ctEntry{1}, {sSubject.Anatomy.FileName});
-                    if iCtIso
-                        iCtVol = intersect(iCtIso, iCtVol);
-                    else
-                        bst_error(sprintf(['The CT that was used to create the IsoSurface cannot be found. ' 10 ...
-                                           'CT file : %s'], ctEntry{1}), 'Loading CT for IsoSurface');
-                        return
-                    end
-                end
-            end
+            iCtVol = GetIsosurfaceData(sSubject, iSrf);
         end
         if ~strcmpi(res, 'mri') && length(iCtVol) > 1
             % Prompt for the CT file selection
@@ -3467,6 +3437,41 @@ function [hFig, iDS, iFig] = DisplayIsosurface(sSubject, iSurface, hFig, Channel
     % SEEG and ECOG: Open tab "iEEG"
     if ismember(Modality, {'SEEG', 'ECOG', 'ECOG+SEEG'})
         gui_brainstorm('ShowToolTab', 'iEEG'); 
+    end
+end
+
+%% ===== FOR AN ISOSURFACE IN A SUBJECT, GET ITS ASSOCIATED CT VOLUME INDEX AND ISOVALUE
+% TODO: do not assume there is only one IsoSurface
+function [iCtVol, isoValue] = GetIsosurfaceData(sSubject, iIsoSurface)
+    % Intialize returned variables
+    iCtVol = [];
+    isoValue = [];
+    % Parse inputs
+    if (nargin < 2) 
+        bst_error('Usage: GetIsosurfaceData(sSubject, iIsoSurface)', 'Get associated CT index and isoValue for IsoSurface');
+        return;
+    end
+    
+    % Load the IsoSurface history
+    sSurf = load(file_fullpath(sSubject.Surface(iIsoSurface).FileName), 'History');
+    if isfield(sSurf, 'History') && ~isempty(sSurf.History)
+        % Get all the CT volumes for the subject
+        iCtVol = find(cellfun(@(x) ~isempty(regexp(x, '_volct', 'match')), {sSubject.Anatomy.FileName}));
+        % Search for CT threshold in history
+        ctEntry  = regexp(sSurf.History{1, 3}, '^Thresholded CT:\s(.*)\sthreshold.*$', 'tokens', 'once');
+        isoValue = regexp(sSurf.History{1, 3}, 'threshold\s*=\s*(\d+)', 'tokens', 'once');
+        isoValue = str2double(isoValue{1});
+        % Return intersection of the found and then update iCtVol
+        if ~isempty(ctEntry)
+            [~, iCtIso] = ismember(ctEntry{1}, {sSubject.Anatomy.FileName});
+            if iCtIso
+                iCtVol = intersect(iCtIso, iCtVol);
+            else
+                bst_error(sprintf(['The CT that was used to create the IsoSurface cannot be found. ' 10 ...
+                                   'CT file : %s'], ctEntry{1}), 'Loading CT for IsoSurface');
+                return
+            end
+        end
     end
 end
 
