80e7f6ea97a839432ca6e8286dac0bcafbcfa172
Chinmay Chinara <chinmay.chinara@gmail.com>
2025-02-13 16:04:33 -0800

    Give a warning switching to `GARDEL` tool

--- toolbox/misc/seeg_contactid_gardel.m
+++ toolbox/misc/seeg_contactid_gardel.m
@@ -38,6 +38,11 @@ if ~isnumeric(iSubject) || (iSubject < 0)
 end
 
 %% ===== START EXTERNAL GARDEL TOOL FROM BRAINSTORM =====
+if ~java_dialog('confirm', ['Warning: Switching from Brainstorm to GARDEL external tool.' 10 ...
+                            'This will hide the Brainstorm GUI.' 10 10 ...
+                            'Do you want to continue?'], 'Start GARDEL tool')
+    return
+end
 % Check for GARDEL plugin installation
 [isInstalledGardel, errMsg] = bst_plugin('Install', 'gardel');
 if ~isInstalledGardel
