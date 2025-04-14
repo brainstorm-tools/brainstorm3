20506d1a92c12258f65be0b6d217f02d8a62a041
rcassani <raymundo.cassani@gmail.com>
2025-04-14 13:13:36 -0400

    Indent and add comments

--- toolbox/gui/panel_import_pet.m
+++ toolbox/gui/panel_import_pet.m
@@ -33,100 +33,109 @@ function varargout = panel_import_pet(varargin)
 % =============================================================================@
 %
 % Authors: Diellor Basha, 2025
-
+%          Raymundo Cassani, 2025
 
 eval(macro_method);
 end
 
 %% ===== CREATE PANEL =====
 function [bstPanelNew, panelName] = CreatePanel(nFrames)
-panelName = 'panel_import_pet';
-import java.awt.*
-import javax.swing.*
-% === Main layout ===
-jPanelMain = gui_river([5, 5], [0, 10, 10, 10]);
-% === FRAME ALIGNMENT & SMOOTHING ===
-if nFrames > 1
-    jPanelAlign = gui_river([2, 2], [0, 10, 10, 10], 'Frame Alignment');
-    gui_component('label', jPanelAlign, 'br', ...
-        sprintf('<HTML><EM>Imported volume contains %d frames.</EM><BR></HTML>', nFrames));
-    jCheckAlign = gui_component('checkbox', jPanelAlign, 'br', 'Align frames');
-    jCheckAlign.setSelected(true);
-    jCheckAverage = gui_component('checkbox', jPanelAlign, 'br', 'Aggregate aligned frames');
-    jCheckAverage.setSelected(true);
-    jPanelSmooth = gui_river([0, 0], [2, 0, 0, 0]);
-    jCheckSmooth = gui_component('checkbox', jPanelSmooth, 'br', 'Apply smoothing');
-    jCheckSmooth.setSelected(false);
-    set(handle(jCheckAlign, 'CallbackProperties'), 'ActionPerformedCallback', ...
-        @(src, evt) jCheckSmooth.setEnabled(jCheckAlign.isSelected()));
-    jPanelFwhm = gui_river([0, 0], [0, 15, 0, 0]);
-    gui_component('label', jPanelFwhm, 'br', 'FWHM (mm): ');
-    jTextFwhm = gui_component('text', jPanelFwhm, 'tab', '8');
-    jTextFwhm.setMaximumSize(java.awt.Dimension(50, 20));
-    jTextFwhm.setEnabled(false);
-    set(handle(jCheckSmooth, 'CallbackProperties'), 'ActionPerformedCallback', ...
-        @(src, evt) jTextFwhm.setEnabled(jCheckSmooth.isSelected()));
-    jPanelSmooth.add('br', jPanelFwhm);
-    jPanelAlign.add('br', jPanelSmooth);
-    jPanelMain.add('br', jPanelAlign);
-else
-    jCheckAlign = [];
-    jCheckAverage = [];
-    jCheckSmooth = [];
-    jTextFwhm = [];
-end
-% === REGISTRATION PANEL ===
-jPanelReg = gui_river('Registration');
-jCheckRegister = gui_component('checkbox', jPanelReg, 'br', 'Register to default MRI');
-jCheckRegister.setSelected(true);
-jCheckReslice = gui_component('checkbox', jPanelReg, 'br', 'Reslice volume on import');
-jCheckReslice.setSelected(true);
-jPanelMain.add('br', jPanelReg);
-% === BUTTONS ===
-jPanelButtons = gui_river([10 0], [6 10 0 10]);
-gui_component('button', jPanelButtons, 'br right', 'Cancel', [], [], @ButtonCancel_Callback);
-gui_component('button', jPanelButtons, '', 'Import', [], [], @ButtonImport_Callback);
-jPanelMain.add('br', jPanelButtons);
-% === Panel Layout  ===
-if ~isempty(jCheckAlign)
-    jPanelAlign.doLayout();
-    jPanelReg.doLayout();
-    maxWidth = max([jPanelAlign.getPreferredSize().width, jPanelReg.getPreferredSize().width]);
-    jPanelAlign.setPreferredSize(java.awt.Dimension(maxWidth, jPanelAlign.getPreferredSize().height));
-    jPanelReg.setPreferredSize(java.awt.Dimension(maxWidth, jPanelReg.getPreferredSize().height));
-end
-% === Create mutex ===
-bst_mutex('create', panelName);
-% === Return panel object ===
-bstPanelNew = BstPanel(panelName, ...
-    jPanelMain, ...
-    struct('jCheckAlign', jCheckAlign, ...
-    'jCheckAverage', jCheckAverage, ...
-    'jCheckSmooth', jCheckSmooth, ...
-    'jTextFwhm', jTextFwhm, ...
-    'jCheckRegister', jCheckRegister, ...
-    'jCheckReslice', jCheckReslice));
-%% ===== BUTTON CALLBACKS =====
+    panelName = 'panel_import_pet';
+    import java.awt.*
+    import javax.swing.*
+    % === Main layout ===
+    jPanelMain = gui_river([5, 5], [0, 10, 10, 10]);
+    % === FRAME ALIGNMENT & SMOOTHING ===
+    if nFrames > 1
+        jPanelAlign = gui_river([2, 2], [0, 10, 10, 10], 'Frame Alignment');
+        gui_component('label', jPanelAlign, 'br', ...
+            sprintf('<HTML><EM>Imported volume contains %d frames.</EM><BR></HTML>', nFrames));
+        jCheckAlign = gui_component('checkbox', jPanelAlign, 'br', 'Align frames');
+        jCheckAlign.setSelected(true);
+        jCheckAverage = gui_component('checkbox', jPanelAlign, 'br', 'Aggregate aligned frames');
+        jCheckAverage.setSelected(true);
+        jPanelSmooth = gui_river([0, 0], [2, 0, 0, 0]);
+        jCheckSmooth = gui_component('checkbox', jPanelSmooth, 'br', 'Apply smoothing');
+        jCheckSmooth.setSelected(false);
+        set(handle(jCheckAlign, 'CallbackProperties'), 'ActionPerformedCallback', ...
+            @(src, evt) jCheckSmooth.setEnabled(jCheckAlign.isSelected()));
+        jPanelFwhm = gui_river([0, 0], [0, 15, 0, 0]);
+        gui_component('label', jPanelFwhm, 'br', 'FWHM (mm): ');
+        jTextFwhm = gui_component('text', jPanelFwhm, 'tab', '8');
+        jTextFwhm.setMaximumSize(java.awt.Dimension(50, 20));
+        jTextFwhm.setEnabled(false);
+        set(handle(jCheckSmooth, 'CallbackProperties'), 'ActionPerformedCallback', ...
+            @(src, evt) jTextFwhm.setEnabled(jCheckSmooth.isSelected()));
+        jPanelSmooth.add('br', jPanelFwhm);
+        jPanelAlign.add('br', jPanelSmooth);
+        jPanelMain.add('br', jPanelAlign);
+    else
+        jCheckAlign = [];
+        jCheckAverage = [];
+        jCheckSmooth = [];
+        jTextFwhm = [];
+    end
+    % === REGISTRATION PANEL ===
+    jPanelReg = gui_river('Registration');
+    jCheckRegister = gui_component('checkbox', jPanelReg, 'br', 'Register to default MRI');
+    jCheckRegister.setSelected(true);
+    jCheckReslice = gui_component('checkbox', jPanelReg, 'br', 'Reslice volume on import');
+    jCheckReslice.setSelected(true);
+    jPanelMain.add('br', jPanelReg);
+    % === BUTTONS ===
+    jPanelButtons = gui_river([10 0], [6 10 0 10]);
+    gui_component('button', jPanelButtons, 'br right', 'Cancel', [], [], @ButtonCancel_Callback);
+    gui_component('button', jPanelButtons, '', 'Import', [], [], @ButtonImport_Callback);
+    jPanelMain.add('br', jPanelButtons);
+    % === Panel Layout  ===
+    if ~isempty(jCheckAlign)
+        jPanelAlign.doLayout();
+        jPanelReg.doLayout();
+        maxWidth = max([jPanelAlign.getPreferredSize().width, jPanelReg.getPreferredSize().width]);
+        jPanelAlign.setPreferredSize(java.awt.Dimension(maxWidth, jPanelAlign.getPreferredSize().height));
+        jPanelReg.setPreferredSize(java.awt.Dimension(maxWidth, jPanelReg.getPreferredSize().height));
+    end
+    % === Create mutex ===
+    bst_mutex('create', panelName);
+    % === Return panel object ===
+    bstPanelNew = BstPanel(panelName, ...
+        jPanelMain, ...
+        struct('jCheckAlign', jCheckAlign, ...
+        'jCheckAverage', jCheckAverage, ...
+        'jCheckSmooth', jCheckSmooth, ...
+        'jTextFwhm', jTextFwhm, ...
+        'jCheckRegister', jCheckRegister, ...
+        'jCheckReslice', jCheckReslice));
+
+%% =================================================================================
+%  === INTERNAL CALLBACKS ==========================================================
+%  =================================================================================
+%% ===== CANCEL BUTTON =====
     function ButtonCancel_Callback(~, ~)
         gui_hide(panelName);
     end
 
+%% ===== IMPORT BUTTON =====
     function ButtonImport_Callback(~, ~)
         bst_mutex('release', panelName);  % Triggers gui_show_dialog to call GetPanelContents
     end
 end
+
+%% =================================================================================
+%  === EXTERNAL CALLBACKS ==========================================================
+%  =================================================================================
 %% ===== GET PANEL CONTENTS =====
 function s = GetPanelContents(h)
-if nargin < 1
-    h = bst_get('PanelControls', 'panel_import_pet');
-end
-s = struct();
-if isfield(h, 'jCheckAlign') && ~isempty(h.jCheckAlign)
-    s.align    = h.jCheckAlign.isSelected();
-    s.average  = h.jCheckAverage.isSelected();
-    s.smooth   = h.jCheckSmooth.isSelected();
-    s.fwhm     = str2double(char(h.jTextFwhm.getText()));
-end
-s.register = h.jCheckRegister.isSelected();
-s.reslice  = h.jCheckReslice.isSelected();
+    if nargin < 1
+        h = bst_get('PanelControls', 'panel_import_pet');
+    end
+    s = struct();
+    if isfield(h, 'jCheckAlign') && ~isempty(h.jCheckAlign)
+        s.align    = h.jCheckAlign.isSelected();
+        s.average  = h.jCheckAverage.isSelected();
+        s.smooth   = h.jCheckSmooth.isSelected();
+        s.fwhm     = str2double(char(h.jTextFwhm.getText()));
+    end
+    s.register = h.jCheckRegister.isSelected();
+    s.reslice  = h.jCheckReslice.isSelected();
 end
