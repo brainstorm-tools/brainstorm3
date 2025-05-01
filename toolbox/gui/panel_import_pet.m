68bbdbc46aa7e11f95fccca3d5a2394abce78991
rcassani <raymundo.cassani@gmail.com>
2025-05-01 15:39:42 -0400

    GUI: Refactor `panel_import_pet.m`
    - Show disabled options for `nFrames` == 1
    - Bugfix: vars were missing in single frame case (NA now as same GUI)
    - Handle dependency of components
    - Callbacks, use `java_setcb` to set Java components CBs

--- toolbox/gui/panel_import_pet.m
+++ toolbox/gui/panel_import_pet.m
@@ -42,90 +42,94 @@ end
 
 %% ===== CREATE PANEL =====
 function [bstPanelNew, panelName] = CreatePanel(nFrames, dispRegistration)
-panelName = 'panel_import_pet';
-import java.awt.*
-import javax.swing.*
-% === Main layout ===
-jPanelMain = gui_river([5, 5], [0, 10, 10, 10]);
-% === FRAME ALIGNMENT & SMOOTHING ===
-if nFrames > 1
-    jPanelAlign = gui_river([2, 2], [0, 10, 10, 10], 'Frame Alignment');
+    panelName = 'panel_import_pet';
+    import java.awt.*
+    import javax.swing.*
+    % Handle number of frames
+    isMultiFrame = nFrames > 1;
+    strFrames = sprintf('%d frame', nFrames);
+    if isMultiFrame
+        strFrames = [strFrames 's'];
+    end
+
+    % === MAIN LAYOUT (2 PANELS) ===
+    jPanelMain = gui_river([5, 5], [0, 10, 10, 10]);
+    % === FRAME ALIGNMENT PANEL (1/2) ===
+    jPanelAlign = gui_river([2, 2], [0, 10, 10, 10], 'Frame alignment');
+    % Alignment
     gui_component('label', jPanelAlign, 'br', ...
-        sprintf('<HTML><EM>Imported volume contains %d frames.</EM><BR></HTML>', nFrames));
+        sprintf('<HTML><EM>Imported volume contains %s.</EM><BR></HTML>', strFrames));
     jCheckAlign = gui_component('checkbox', jPanelAlign, 'br', 'Align frames');
-    jCheckAlign.setSelected(true);
-
-    jPanelAggregate = gui_river([0, 0], [2, 0, 0, 0]);
-    jCheckAggregate = gui_component('checkbox', jPanelAggregate, 'br', 'Aggregate frames: ');
-    jCheckAggregate.setSelected(true);
-    jComboboxAggregate = gui_component('combobox', jPanelAggregate, 'tab', [], {{'Mean', 'Sum', 'Median', 'Max', 'Min', 'First', 'Last', 'Z-score'}});
-    set(handle(jCheckAggregate, 'CallbackProperties'), 'ActionPerformedCallback', ...
-        @(src, evt) jComboboxAggregate.setEnabled(jCheckAggregate.isSelected()));
-
+    jCheckAlign.setSelected(isMultiFrame);
+    % Smooth
     jPanelSmooth = gui_river([0, 0], [2, 0, 0, 0]);
     jCheckSmooth = gui_component('checkbox', jPanelSmooth, 'br', 'Apply smoothing');
-    jCheckSmooth.setSelected(false);
-    set(handle(jCheckAlign, 'CallbackProperties'), 'ActionPerformedCallback', ...
-        @(src, evt) jCheckSmooth.setEnabled(jCheckAlign.isSelected()));
+    jCheckSmooth.setSelected(isMultiFrame);
+    jCheckSmooth.setEnabled(isMultiFrame);
     jPanelFwhm = gui_river([0, 0], [0, 15, 0, 0]);
-    gui_component('label', jPanelFwhm, 'br', 'FWHM (mm): ');
+    jLabelFwhm = gui_component('label', jPanelFwhm, 'br', 'FWHM (mm): ');
     jTextFwhm = gui_component('text', jPanelFwhm, 'tab', '8');
     jTextFwhm.setMaximumSize(java.awt.Dimension(50, 20));
-    jTextFwhm.setEnabled(false);
-    set(handle(jCheckSmooth, 'CallbackProperties'), 'ActionPerformedCallback', ...
-        @(src, evt) jTextFwhm.setEnabled(jCheckSmooth.isSelected()));
+    SetEnabled([jTextFwhm, jLabelFwhm], jCheckSmooth.isSelected());
+    java_setcb(jCheckSmooth, 'ActionPerformedCallback', @(h, ev)SetEnabled([jTextFwhm, jLabelFwhm], jCheckSmooth.isSelected()));
+    java_setcb(jCheckAlign, 'ActionPerformedCallback', @(h, ev)SetSelectedAndEnabled(jCheckSmooth, jCheckSmooth.isSelected() && jCheckAlign.isSelected(), jCheckAlign.isSelected));
     jPanelSmooth.add('br', jPanelFwhm);
     jPanelAlign.add('br', jPanelSmooth);
+    % Aggregate
+    jPanelAggregate = gui_river([0, 0], [2, 0, 0, 0]);
+    jCheckAggregate = gui_component('checkbox', jPanelAggregate, 'br', 'Aggregate frames: ');
+    jCheckAggregate.setSelected(isMultiFrame);
+    jComboboxAggregate = gui_component('combobox', jPanelAggregate, 'tab', [], {{'Mean', 'Sum', 'Median', 'Max', 'Min', 'First', 'Last', 'Z-score'}});
+    jComboboxAggregate.setEnabled(isMultiFrame);
+    java_setcb(jCheckAggregate, 'ActionPerformedCallback', @(h, ev)jComboboxAggregate.setEnabled(jCheckAggregate.isSelected()));
     jPanelAlign.add('br', jPanelAggregate);
+    % Disable entries if single frame
+    if ~isMultiFrame
+        SetEnabled([jPanelAlign, jCheckAlign, jPanelSmooth, jCheckSmooth, jPanelAggregate, jCheckAggregate], 0);
+    end
+    % === REGISTRATION PANEL (2/2) ===
+    jPanelReg = gui_river('Registration');
+    jCheckRegister = gui_component('checkbox', jPanelReg, 'br', 'Register to MRI using:');
+    jCheckRegister.setSelected(true);
+    jComboboxRegister = gui_component('combobox', jPanelReg, 'tab', [], {{'SPM', 'MNI'}});
+    java_setcb(jCheckRegister, 'ActionPerformedCallback', @(h, ev)jComboboxRegister.setEnabled(jCheckRegister.isSelected()));
+    jCheckReslice = gui_component('checkbox', jPanelReg, 'br', 'Reslice volume on import');
+    jCheckReslice.setSelected(true);
+    if ~dispRegistration
+        jPanelReg.setVisible(0);
+        jCheckRegister.setSelected(0);
+        jCheckReslice.setSelected(0);
+    end
+    % Add panels to main layout
     jPanelMain.add('br', jPanelAlign);
-else
-    jCheckAlign = [];
-    jCheckAggregate = [];
-    jCheckSmooth = [];
-    jTextFwhm = [];
-end
-% === REGISTRATION PANEL ===
-jPanelReg = gui_river('Registration');
-jCheckRegister = gui_component('checkbox', jPanelReg, 'br', 'Register to MRI using:');
-jCheckRegister.setSelected(true);
-jComboboxRegister = gui_component('combobox', jPanelReg, 'tab', [], {{'SPM', 'MNI'}});
-    set(handle(jCheckRegister, 'CallbackProperties'), 'ActionPerformedCallback', ...
-        @(src, evt) jComboboxRegister.setEnabled(jCheckRegister.isSelected()));
-jCheckReslice = gui_component('checkbox', jPanelReg, 'br', 'Reslice volume on import');
-jCheckReslice.setSelected(true);
-jPanelMain.add('br', jPanelReg);
-if ~dispRegistration
-    jPanelReg.setVisible(0);
-    jCheckRegister.setSelected(0);
-    jCheckReslice.setSelected(0);
-end
+    jPanelMain.add('br', jPanelReg);
 
-% === BUTTONS ===
-jPanelButtons = gui_river([2 0], [0 5 0 5]);
-gui_component('button', jPanelButtons, 'br right', 'Cancel', [], [], @ButtonCancel_Callback);
-gui_component('button', jPanelButtons, '', 'Import', [], [], @ButtonImport_Callback);
-jPanelMain.add('br right', jPanelButtons);
-% === Panel Layout  ===
-if ~isempty(jCheckAlign)
-    jPanelAlign.doLayout();
-    jPanelReg.doLayout();
-    maxWidth = max([jPanelAlign.getPreferredSize().width, jPanelReg.getPreferredSize().width]);
-    jPanelAlign.setPreferredSize(java.awt.Dimension(maxWidth, jPanelAlign.getPreferredSize().height));
-    jPanelReg.setPreferredSize(java.awt.Dimension(maxWidth, jPanelReg.getPreferredSize().height));
-end
-% === Create mutex ===
-bst_mutex('create', panelName);
-% === Return panel object ===
-bstPanelNew = BstPanel(panelName, ...
-    jPanelMain, ...
-    struct('jCheckAlign', jCheckAlign, ...
-    'jCheckAggregate', jCheckAggregate, ...
-    'jComboBoxAggregate', jComboboxAggregate, ...
-    'jCheckSmooth', jCheckSmooth, ...
-    'jTextFwhm', jTextFwhm, ...
-    'jCheckRegister', jCheckRegister, ...
-    'jComboboxRegister', jComboboxRegister, ...
-    'jCheckReslice', jCheckReslice));
+    % === BUTTONS ===
+    jPanelButtons = gui_river([2 0], [0 5 0 5]);
+    gui_component('button', jPanelButtons, 'br right', 'Cancel', [], [], @ButtonCancel_Callback);
+    gui_component('button', jPanelButtons, '', 'Import', [], [], @ButtonImport_Callback);
+    jPanelMain.add('br right', jPanelButtons);
+    % === Panel Layout  ===
+    if ~isempty(jCheckAlign)
+        jPanelAlign.doLayout();
+        jPanelReg.doLayout();
+        maxWidth = max([jPanelAlign.getPreferredSize().width, jPanelReg.getPreferredSize().width]);
+        jPanelAlign.setPreferredSize(java.awt.Dimension(maxWidth, jPanelAlign.getPreferredSize().height));
+        jPanelReg.setPreferredSize(java.awt.Dimension(maxWidth, jPanelReg.getPreferredSize().height));
+    end
+    % === Create mutex ===
+    bst_mutex('create', panelName);
+    % === Return panel object ===
+    bstPanelNew = BstPanel(panelName, ...
+        jPanelMain, ...
+        struct('jCheckAlign',        jCheckAlign, ...
+               'jCheckAggregate',    jCheckAggregate, ...
+               'jComboBoxAggregate', jComboboxAggregate, ...
+               'jCheckSmooth',       jCheckSmooth, ...
+               'jTextFwhm',          jTextFwhm, ...
+               'jCheckRegister',     jCheckRegister, ...
+               'jComboboxRegister',  jComboboxRegister, ...
+               'jCheckReslice',      jCheckReslice));
 
 %% =================================================================================
 %  === INTERNAL CALLBACKS ==========================================================
@@ -166,4 +170,20 @@ function s = GetPanelContents()
         s.register = 'ignore';
     end
     s.reslice  = ctrl.jCheckReslice.isSelected();
+end
+
+%% ===== SET ENABLED FOR COMPONENTS =====
+function SetEnabled(components, status)
+    for iComponent = 1 : length(components)
+        components(iComponent).setEnabled(status);
+    end
+end
+
+%% ===== SET SELECTED AND ENABLED FOR CHECK COMPONENTS =====
+function SetSelectedAndEnabled(component, isSelected, isEnabled)
+    % If value changed, doClick, it triggers ActionPerformedCallback
+    if component.isSelected ~= isSelected
+        component.doClick;
+    end
+    component.setEnabled(isEnabled);
 end
