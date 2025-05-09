0ac389166ec48d44ce76a7d4d30113a3386f6888
Anna Zaidi <annazaidi@annas-air.home>
2025-05-08 21:21:45 -0400

    Add HED tagging infrastructure

---
+++ HED_integration/testCTagger.m
@@ -0,0 +1,20 @@
+% Load the events.json file
+eventsJsonFile = 'task-FacePerception_events.json';
+
+if exist(eventsJsonFile, 'file')
+    disp('Using events.json as input...');
+    sidecar = eventsJsonFile;
+else
+    sidecar = '';
+end
+
+%example data structure
+dataStruct.events = struct('onset', {0.5, 1.0, 1.5}, ...
+                           'duration', {0.5, 0.5, 0.5}, ...
+                           'trial_type', {'stimulus', 'response', 'stimulus'});
+
+% Integrate CTAGGER
+[dataStruct, tags] = integrateCTAGGER(dataStruct, 'sidecar', sidecar);
+
+disp('Updated JSON:');
+disp(tags);
