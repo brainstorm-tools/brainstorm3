631d9815b2b8e9c392751effe98419b918218063
rcassani <raymundo.cassani@gmail.com>
2025-06-03 14:53:42 -0400

    Update `autocomment` in new files

--- toolbox/anatomy/mri_mask.m
+++ toolbox/anatomy/mri_mask.m
@@ -20,6 +20,24 @@ function [atlasLabels, MriFileMask, errMsg, fileTag, binMask] = mri_mask(MriFile
 %   - fileTag     : File tag used for output file
 %   - binMask     : Binary mask used for masking (if doMask==1), otherwise []
 %
+% @=============================================================================
+% This function is part of the Brainstorm software:
+% https://neuroimage.usc.edu/brainstorm
+% 
+% Copyright (c) University of Southern California & McGill University
+% This software is distributed under the terms of the GNU General Public License
+% as published by the Free Software Foundation. Further details on the GPLv3
+% license can be found at http://www.gnu.org/copyleft/gpl.html.
+% 
+% FOR RESEARCH PURPOSES ONLY. THE SOFTWARE IS PROVIDED "AS IS," AND THE
+% UNIVERSITY OF SOUTHERN CALIFORNIA AND ITS COLLABORATORS DO NOT MAKE ANY
+% WARRANTY, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO WARRANTIES OF
+% MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, NOR DO THEY ASSUME ANY
+% LIABILITY OR RESPONSIBILITY FOR THE USE OF THIS SOFTWARE.
+%
+% For more information type "brainstorm license" at command prompt.
+% =============================================================================@
+%
 % Authors: Diellor Basha, 2025
 
 % Defaults
--- toolbox/gui/panel_process_pet.m
+++ toolbox/gui/panel_process_pet.m
@@ -7,6 +7,26 @@ function varargout = panel_process_pet(varargin)
 %   1. Select an atlas (dropdown, populated by bst_get('AtlasFile'))
 %   2. Select an ROI (dropdown, populated by mri_mask([], AtlasName))
 %   3. Optionally rescale or mask a PET volume using the selected ROI
+%
+% @=============================================================================
+% This function is part of the Brainstorm software:
+% https://neuroimage.usc.edu/brainstorm
+% 
+% Copyright (c) University of Southern California & McGill University
+% This software is distributed under the terms of the GNU General Public License
+% as published by the Free Software Foundation. Further details on the GPLv3
+% license can be found at http://www.gnu.org/copyleft/gpl.html.
+% 
+% FOR RESEARCH PURPOSES ONLY. THE SOFTWARE IS PROVIDED "AS IS," AND THE
+% UNIVERSITY OF SOUTHERN CALIFORNIA AND ITS COLLABORATORS DO NOT MAKE ANY
+% WARRANTY, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO WARRANTIES OF
+% MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, NOR DO THEY ASSUME ANY
+% LIABILITY OR RESPONSIBILITY FOR THE USE OF THIS SOFTWARE.
+%
+% For more information type "brainstorm license" at command prompt.
+% =============================================================================@
+%
+% Authors: Diellor Basha, 2025
 
 eval(macro_method);
 end
