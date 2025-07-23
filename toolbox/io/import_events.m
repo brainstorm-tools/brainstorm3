brainstorm-tools/brainstorm3

commit f38716ec77ee08b751b85a42f2a91c26dc544c8f

IO: Merge channel info for simultaneous events with same event name

doc/license.html
@@ -5,7 +5,7 @@
 <body alink="#fff000" link="#fff000" vlink="#fff000">
     <h4><span style="font-family: Arial Black; color: #ffffff;"><strong>THERE IS NO UNDO BUTTON!<BR>SET UP A <FONT color=red>BACKUP</FONT> OF YOUR DATABASE</strong></span></h4>
 <HR>
-<!-- LICENCE_START -->Version: 3.250721 (21-Jul-2025)<br>
+<!-- LICENCE_START -->Version: 3.250722 (22-Jul-2025)<br>
 <span style="font-style: italic;">COPYRIGHT &copy; 2000-2025
 USC &amp; McGill University.<br>
 </span>

toolbox/io/import_events.m
@@ -296,6 +296,38 @@ for iNew = 1:length(newEvents)
     end
 end
 
+% Merge channels for simultaneous channelwise events
+for iEvt = 1:length(sFile.events)
+    iOcc = find(~cellfun(@isempty, sFile.events(iEvt).channels));
+    if numel(iOcc) < 2
+        % Noting to merge
+        continue
+    end
+    % Only for events with channel info
+    [~, ics, ias] = unique(bst_round(sFile.events(iEvt).times(:,iOcc)', 9), 'rows', 'stable');
+    for ic = 1 : numel(ics)
+        irep = iOcc(ias == ic);
+        if numel(irep) < 2
+            continue
+        end
+        % Merge channels if there are not Notes, or all Notes are the same for repeated occurrences
+        if isempty(sFile.events(iEvt).notes) || isequal(sFile.events(iEvt).notes{irep})
+            % Merge channels in first occurrence
+            sFile.events(iEvt).channels{irep(1)} = unique([sFile.events(iEvt).channels{irep}]);
+            % Delete merged occurences
+            sFile.events(iEvt).channels(irep(2:end)) = [];
+            sFile.events(iEvt).times(:, irep(2:end)) = [];
+            sFile.events(iEvt).epochs(irep(2:end))   = [];
+            if ~isempty(sFile.events(iEvt).notes)
+                sFile.events(iEvt).notes(irep(2:end)) = [];
+            end
+            if ~isempty(sFile.events(iEvt).reactTimes)
+                sFile.events(iEvt).reactTimes(irep(2:end)) = [];
+            end
+        end
+    end
+end
+
 % %% ===== SORT EVENTS BY LABEL =====
 % [tmp__, iSort] = sort({sFile.events.label});
 % sFile.events = sFile.events(iSort);

commit e357dd149794cab06bf4944a16fa28b537c381cc

Bugfix: Bug at merging channel info for simultaneous events
Introduced in f38716e
Reported: https://neuroimage.usc.edu/forums/t/52550

doc/license.html
@@ -5,7 +5,7 @@
 <body alink="#fff000" link="#fff000" vlink="#fff000">
     <h4><span style="font-family: Arial Black; color: #ffffff;"><strong>THERE IS NO UNDO BUTTON!<BR>SET UP A <FONT color=red>BACKUP</FONT> OF YOUR DATABASE</strong></span></h4>
 <HR>
-<!-- LICENCE_START -->Version: 3.250722 (22-Jul-2025)<br>
+<!-- LICENCE_START -->Version: 3.250723 (23-Jul-2025)<br>
 <span style="font-style: italic;">COPYRIGHT &copy; 2000-2025
 USC &amp; McGill University.<br>
 </span>

toolbox/io/import_events.m
@@ -298,6 +298,10 @@ end
 
 % Merge channels for simultaneous channelwise events
 for iEvt = 1:length(sFile.events)
+    if isempty(sFile.events(iEvt).channels)
+        % Noting to merge
+        continue
+    end
     iOcc = find(~cellfun(@isempty, sFile.events(iEvt).channels));
     if numel(iOcc) < 2
         % Noting to merge