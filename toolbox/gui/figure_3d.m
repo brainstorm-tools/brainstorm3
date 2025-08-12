bc2152114923434071b8789cc4a7edc98c53563b
Chinmay Chinara <chinmay.chinara@gmail.com>
2025-08-12 10:54:41 -0700

    Revert "Surface smoothing not required for Isosurface"
    This reverts commit 95d5a616104f46c12c9b1855bbf3924d467ff960.

--- toolbox/gui/figure_3d.m
+++ toolbox/gui/figure_3d.m
@@ -3297,8 +3297,8 @@ end
 function SmoothSurface(hFig, iTess, smoothValue)
     % Get surfaces list 
     TessInfo = getappdata(hFig, 'Surface');
-    % Ignore MRI slices or Isosurface
-    if ismember(TessInfo(iTess).Name, {'Anatomy', 'FEM'}) || ~isempty(regexp(TessInfo(iTess).SurfaceFile, 'tess_isosurface', 'match'))
+    % Ignore MRI slices
+    if ismember(TessInfo(iTess).Name, {'Anatomy', 'FEM'})
         return
     end
     % Get surfaces vertices
