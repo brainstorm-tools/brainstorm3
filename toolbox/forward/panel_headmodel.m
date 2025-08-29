brainstorm-tools/brainstorm3

commit 3a2494bf335f7b39da1c421a7a7797b99df98338

Remove duplicated code

toolbox/forward/panel_headmodel.m
@@ -166,8 +166,7 @@ function [bstPanelNew, panelName] = CreatePanel(isMeg, isEeg, isEcog, isSeeg, is
                   'jCheckMethodSEEG',    jCheckMethodSEEG, ...
                   'jComboMethodSEEG',    jComboMethodSEEG, ...
                   'jCheckMethodNIRS',    jCheckMethodNIRS, ...
-                  'jComboMethodNIRS',    jComboMethodNIRS);
-
+                 );
     % Create the BstPanel object that is returned by the function
     bstPanelNew = BstPanel(panelName, jPanelNew, ctrl);
 
@@ -295,11 +294,6 @@ function s = GetPanelContents() %#ok<DEFNU>
         s.ECOGMethod = char(ctrl.jComboMethodECOG.getSelectedItem.getType());
     else
         s.ECOGMethod = '';
-    end
-    if ~isempty(ctrl.jCheckMethodSEEG) && ctrl.jCheckMethodSEEG.isSelected()
-        s.SEEGMethod = char(ctrl.jComboMethodSEEG.getSelectedItem.getType());
-    else
-        s.SEEGMethod = '';
     end
     if ~isempty(ctrl.jCheckMethodSEEG) && ctrl.jCheckMethodSEEG.isSelected()
         s.SEEGMethod = char(ctrl.jComboMethodSEEG.getSelectedItem.getType());

commit ca18724d6ad4fc469bbd37986dcd74614200728d

Silly mistake, introduced in 3a2494b

toolbox/forward/panel_headmodel.m
@@ -164,7 +164,8 @@ function [bstPanelNew, panelName] = CreatePanel(isMeg, isEeg, isEcog, isSeeg, is
                   'jCheckMethodECOG',    jCheckMethodECOG, ...
                   'jComboMethodECOG',    jComboMethodECOG, ...
                   'jCheckMethodSEEG',    jCheckMethodSEEG, ...
-                  'jCheckMethodNIRS',    jCheckMethodNIRS ...
+                  'jCheckMethodNIRS',    jCheckMethodNIRS, ...
+                  'jComboMethodNIRS',    jComboMethodNIRS ...
                  );
     % Create the BstPanel object that is returned by the function
     bstPanelNew = BstPanel(panelName, jPanelNew, ctrl);