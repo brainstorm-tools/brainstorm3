brainstorm-tools/brainstorm3

commit 11109c753e8a3898968a8864612ec8fa1e4410c2

Events: Improve merging of simultaneous event occurences

toolbox/process/functions/process_evt_merge.m
@@ -201,36 +201,23 @@ function [events, isModified] = Compute(sInput, events, EvtNames, NewName, isDel
         end
         newEvent.reactTimes = [events(iEvents).reactTimes];
     end
-    % Find duplicated events
-    iRemoveDuplicate = [];
-    [~, ics, ias] = unique(bst_round(newEvent.times', 9), 'rows', 'stable');
-    % Check if duplicated times are really duplicated events
-    for ix = 1 : length(ics)
-        ids = find(ias == ix);
-        for iy = 2 : length(ids)
-            id = ids(iy);
-            if (isempty(newEvent.channels)   || isequal(newEvent.channels{ids(1)}, newEvent.channels{id})) && ...
-               (isempty(newEvent.notes)      || isequal(newEvent.notes{ids(1)}, newEvent.notes{id})) && ...
-               (isempty(newEvent.reactTimes) || isequal(newEvent.reactTimes(ids(1)), newEvent(id).reactTimes))
-               iRemoveDuplicate = [iRemoveDuplicate, id];
-            end
+    % Sort by time
+    if (size(newEvent.times, 2) > 1)
+        [tmp__, iSort] = sort(bst_round(newEvent.times(1,:), 9));
+        newEvent.times   = snewEvent.times(:,iSort);
+        newEvent.epochs  = newEvent.epochs(iSort);
+        if ~isempty(newEvent.reactTimes)
+            newEvent.reactTimes = newEvent.reactTimes(iSort);
+        end
+        if ~isempty(newEvent.channels)
+            newEvent.channels = newEvent.channels(iSort);
+        end
+        if ~isempty(newEvent.notes)
+            newEvent.notes = newEvent.notes(iSort);
         end
     end
-    % Sort by samples indices
-    [~, iSort] = sort(bst_round(newEvent.times(1,:), 9));
-    % Remove indices of duplicated events
-    iSort = iSort(~ismember(iSort, iRemoveDuplicate));
-    newEvent.times    = newEvent.times(:,iSort);
-    newEvent.epochs   = newEvent.epochs(iSort);
-    if ~isempty(newEvent.channels)
-        newEvent.channels = newEvent.channels(iSort);
-    end
-    if ~isempty(newEvent.notes)
-        newEvent.notes = newEvent.notes(iSort);
-    end
-    if ~isempty(newEvent.reactTimes)
-        newEvent.reactTimes = newEvent.reactTimes(iSort);
-    end
+    % Merge simultaneous event occurences
+    newEvent = MergeOccurrences(newEvent);
     % Return events to its original state
     events = eventsOrg;
     % Remove merged events
@@ -242,11 +229,61 @@ function [events, isModified] = Compute(sInput, events, EvtNames, NewName, isDel
     end
     % Add new event
     events(end + 1) = newEvent;
-
     % File was modified
     isModified = 1;
 end
 
+
+%% ===== MERGE REPEATED OCCURRENCES =====
+function sEvents = MergeOccurrences(sEvents)
+    % Indices of repeated occurences, aka indices to delete after merge
+    idel = [];
+    % Simultaneous occurences (checks simple and extended event types)
+    [~, ics, ias] = unique(bst_round(sEvents.times', 9), 'rows', 'stable');
+    for ic = 1 : numel(ics)
+        irep = find(ias == ic);
+        if numel(irep) < 2
+            continue
+        end
+        % Merge event occurences IFF: there are not Notes OR all Notes are the same for repeated occurrences
+        if isempty(sEvents.notes) || isequal(sEvents.notes{irep})
+            % Check channel info
+            if isempty(sEvents.channels)
+                irepNoChannel = irep;
+                irepChannel   = [];
+            else
+                irepNoChannel = cellfun(@isempty, sEvents.channels(irep));
+                irepChannel   = ~irepNoChannel;
+                irepNoChannel = irep(irepNoChannel);
+                irepChannel   = irep(irepChannel);
+            end
+            % Merge occurences without channel information
+            if length(irepNoChannel) >= 2
+                % Mark repeated occurences to be deleted
+                idel = [idel, irepNoChannel(2:end)];
+            end
+            % Merge occurences with channel information
+            if length(irepChannel) >= 2
+                % Merge channel list in first occurence of repeated the occurences
+                sEvents.channels{irepChannel(1)} = unique([sEvents.channels{irepChannel}]);
+                % Mark repeated occurence to be deleted
+                idel = [idel, irepChannel(2:end)];
+            end
+        end
+    end
+    % Delete merged occurences
+    sEvents.times(:, idel) = [];
+    sEvents.epochs(idel)   = [];
+    if ~isempty(sEvents.channels)
+        sEvents.channels(idel) = [];
+    end
+    if ~isempty(sEvents.notes)
+        sEvents.notes(idel) = [];
+    end
+    if ~isempty(sEvents.reactTimes)
+        sEvents.reactTimes(idel) = [];
+    end
+end
 
 
 

doc/license.html
@@ -5,7 +5,7 @@
 <body alink="#fff000" link="#fff000" vlink="#fff000">
     <h4><span style="font-family: Arial Black; color: #ffffff;"><strong>THERE IS NO UNDO BUTTON!<BR>SET UP A <FONT color=red>BACKUP</FONT> OF YOUR DATABASE</strong></span></h4>
 <HR>
-<!-- LICENCE_START -->Version: 3.250918 (18-Sep-2025)<br>
+<!-- LICENCE_START -->Version: 3.250919 (19-Sep-2025)<br>
 <span style="font-style: italic;">COPYRIGHT &copy; 2000-2025
 USC &amp; McGill University.<br>
 </span>

toolbox/core/bst_get.m
@@ -3875,7 +3875,7 @@ switch contextName
                     {'.txt'},          'Array of samples (*.txt)',     'ARRAY-SAMPLES'; ...
                     {'.txt','.csv'},   'CSV text file: label, time, duration (*.txt;*.csv)', 'CSV-TIME'; ...
                     {'.txt'},          'CTF Video Times (*.txt)',      'CTFVIDEO'; ...
-                    {'_events.tsv'},   'BIDS events: onset, duration, trial_type (*_events.tsv)', 'BIDS'; ...
+                    {'_events.tsv'},   'BIDS events: onset, duration, trial_type, channel (*_events.tsv)', 'BIDS'; ...
                     };
             case 'channel'
                 argout1 = {...

toolbox/io/import_events.m
@@ -250,7 +250,7 @@ for iNew = 1:length(newEvents)
         sFile.events(iEvt).notes      = [sFile.events(iEvt).notes, newEvents(iNew).notes];
         % Sort by time
         if (size(sFile.events(iEvt).times, 2) > 1)
-            [tmp__, iSort] = unique(bst_round(sFile.events(iEvt).times(1,:), 9));
+            [tmp__, iSort] = sort(bst_round(sFile.events(iEvt).times(1,:), 9));
             sFile.events(iEvt).times   = sFile.events(iEvt).times(:,iSort);
             sFile.events(iEvt).epochs  = sFile.events(iEvt).epochs(iSort);
             if ~isempty(sFile.events(iEvt).reactTimes)
@@ -296,40 +296,9 @@ for iNew = 1:length(newEvents)
     end
 end
 
-% Merge channels for simultaneous channelwise events
+% Merge simultaneous event occurences
 for iEvt = 1:length(sFile.events)
-    if isempty(sFile.events(iEvt).channels)
-        % Noting to merge
-        continue
-    end
-    iOcc = find(~cellfun(@isempty, sFile.events(iEvt).channels));
-    if numel(iOcc) < 2
-        % Noting to merge
-        continue
-    end
-    % Only for events with channel info
-    [~, ics, ias] = unique(bst_round(sFile.events(iEvt).times(:,iOcc)', 9), 'rows', 'stable');
-    for ic = 1 : numel(ics)
-        irep = iOcc(ias == ic);
-        if numel(irep) < 2
-            continue
-        end
-        % Merge channels if there are not Notes, or all Notes are the same for repeated occurrences
-        if isempty(sFile.events(iEvt).notes) || isequal(sFile.events(iEvt).notes{irep})
-            % Merge channels in first occurrence
-            sFile.events(iEvt).channels{irep(1)} = unique([sFile.events(iEvt).channels{irep}]);
-            % Delete merged occurences
-            sFile.events(iEvt).channels(irep(2:end)) = [];
-            sFile.events(iEvt).times(:, irep(2:end)) = [];
-            sFile.events(iEvt).epochs(irep(2:end))   = [];
-            if ~isempty(sFile.events(iEvt).notes)
-                sFile.events(iEvt).notes(irep(2:end)) = [];
-            end
-            if ~isempty(sFile.events(iEvt).reactTimes)
-                sFile.events(iEvt).reactTimes(irep(2:end)) = [];
-            end
-        end
-    end
+    sFile.events(iEvt) = process_evt_merge('MergeOccurrences', sFile.events(iEvt));
 end
 
 % %% ===== SORT EVENTS BY LABEL =====

toolbox/io/in_events_csv.m
@@ -58,7 +58,7 @@ for iEvt = 1:length(uniqueEvents)
         continue;
     end
     % Simple events
-    if (length(csvValues) < 3) || any(isnan(csvValues{3}))
+    if (length(csvValues) < 3) || any(isnan(csvValues{3})) || all(csvValues{3} == 0)
         evtTimes = csvValues{2}(iSmp)';
     else
     % Extended events

commit 30993d8077ad8eb1b040e4fe8078894d725913dd

Typo introduced in 11109c7

toolbox/process/functions/process_evt_merge.m
@@ -204,7 +204,7 @@ function [events, isModified] = Compute(sInput, events, EvtNames, NewName, isDel
     % Sort by time
     if (size(newEvent.times, 2) > 1)
         [tmp__, iSort] = sort(bst_round(newEvent.times(1,:), 9));
-        newEvent.times   = snewEvent.times(:,iSort);
+        newEvent.times   = newEvent.times(:,iSort);
         newEvent.epochs  = newEvent.epochs(iSort);
         if ~isempty(newEvent.reactTimes)
             newEvent.reactTimes = newEvent.reactTimes(iSort);