d2753694d2f88d484492cd55fe7f10c0c65e57b4
Chinmay Chinara <chinmay.chinara@gmail.com>
2025-09-30 12:18:55 -0700

    Add `resection-labeling` plugin

--- toolbox/core/bst_plugin.m
+++ toolbox/core/bst_plugin.m
@@ -202,6 +202,26 @@ function PlugDesc = GetSupported(SelPlug, UserDefVerbose)
     PlugDesc(end).ReadmeFile     = 'README.md';
     PlugDesc(end).LoadFolders    = {'*'};
     PlugDesc(end).TestFile       = 'process_nmp_fetch_maps.m';
+    
+    % === ANATOMY: RESECTION LABELING ===
+    PlugDesc(end+1)              = GetStruct('resection-labeling');
+    PlugDesc(end).Version        = 'latest';
+    PlugDesc(end).Category       = 'Anatomy';
+    PlugDesc(end).AutoUpdate     = 1;
+    switch(OsType)
+        case 'linux64'
+            PlugDesc(end).URLzip   = 'https://neuroimage.usc.edu/bst/getupdate.php?d=bst_resection_labeling_linux.zip';
+            PlugDesc(end).TestFile = 'resection_labeling';
+        case {'mac64', 'mac64arm'}
+            PlugDesc(end).URLzip   = 'https://neuroimage.usc.edu/bst/getupdate.php?d=bst_resection_labeling_mac.zip';
+            PlugDesc(end).TestFile = 'resection_labeling';
+        case 'win64'
+            PlugDesc(end).URLzip   = 'https://neuroimage.usc.edu/bst/getupdate.php?d=bst_resection_labeling_win.zip';
+            PlugDesc(end).TestFile = 'resection_labeling.exe';
+    end
+    PlugDesc(end).URLinfo        = 'https://github.com/ajoshiusc/auto_resection_mask/tree/brainstorm-plugin';
+    PlugDesc(end).CompiledStatus = 1;
+    PlugDesc(end).LoadFolders    = {'bin'};
 
     % === ANATOMY: ROAST ===
     PlugDesc(end+1)              = GetStruct('roast');
