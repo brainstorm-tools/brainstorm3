636810687625f48fb542ffc652c5c930f74d82a8
Edouard2laire <edouard.delaire@icloud.com>
2025-09-30 11:44:49 -0400

    [IO] Export fiducials (#844)
    * Add function to export MRI fiducials
    
    automatically export the fiducials
    
    Apply the required coordinate system
    
    Round coordinate if coordinate system is voxel
    
    Dont convert the coordiate
    
    working prototype
    
    only export fiducials when exporting BIDS
    
    refactor
    
    ready for review
    
    only keep BIDS usage
    
    * revert change on export_mri
    
    * correct for voxel size
    
    * move function to export_bids
    
    * move function to export_mri
    
    * ask if the fiducials should be exported
    
    * move error to export_fiducials_bids
    
    * Use `bst_fullfile` and `bst_jsonencode`. Update comments
    
    * AnatomicalLandmarkCoordinates instead of  FiducialsCoordinates

--- toolbox/process/functions/process_export_bids.m
+++ toolbox/process/functions/process_export_bids.m
@@ -426,7 +426,7 @@ function sInputs = Run(sProcess, sInputs) %#ok<DEFNU>
                     else
                         origMri = sSubject.Anatomy(1).FileName;
                     end
-                    export_mri(origMri, mriFile);
+                    export_mri(origMri, mriFile, 'bids');
                     mriGzFile = gzip(mriFile);
                     if ~isempty(mriGzFile)
                         delete(mriFile);
@@ -1128,4 +1128,3 @@ function defacedMri = GetDefacedMri(sSubject)
         end
     end
 end
-
