brainstorm-tools/brainstorm3

commit b00f3d0d22b8ae1b7a60644575855d5e586cf951

[Head model] Add NIRS mehotd in GUI and process (#815)
* prepare nirs head model

* More preparation

* first working prove of concept

* Add panel for options

* ready for review

* Also adapt the process

* Add field `NIRSMethod`: in `hm.mat` and `Study.HeadModel` (database)

* Empty lines in `bst_headmodeler.m`

* Changes to call NIRSTORM headmodeler

- Check for correct NIRSTORM version
- Get sSubject with `bst_get()`
- Code format

* Update GUI in `panel_headmodel_nirstorm`

* Remove extra empty lines

* Remove duplicated code

* Typos and empty lines

* Minor changes

* Select NIRS checkbox if there are NIRS sensors

* Trivial

* Silly mistake, introduced in 3a2494b

* Update doc string

* Improved panel to get fluence data source

* Add author

* Add error message when trying to compute non-surface head model for nirs

* disable nirs from panel when using volumetric head model

* Get `PlugDesc` on checking plugin installation

* Force one selected modality if it's unique

* Disable NIRS option for volumetric and mixed head model

* Cannot reach this statement (not needed)

* Bump DB version, so DBs without the field `NIRSMethod` will be updated

---------

Co-authored-by: rcassani <raymundo.cassani@gmail.com>

toolbox/db/db_update.m
@@ -353,6 +353,23 @@ if (CurrentDbVersion < 5.02)
     disp('BST> Database update: Done.');
 end
 
+%% ===== UPDATES: 2-Sep-2025 =====
+% Function to update structure
+    function sStudy = AddNirsMethod(sStudy)
+    if ~isempty(sStudy.HeadModel)
+        [sStudy.HeadModel.NIRSMethod] = deal('');
+    else
+        sStudy.Data = repmat(db_template('headmodel'), 0);
+    end
+end
+% Modification: Add 'NIRSMethod' to the Headmodel structure
+if (CurrentDbVersion < 5.03) && ~isempty(ProtocolsListStudies) && ~isfield(ProtocolsListStudies(1).AnalysisStudy.HeadModel, 'NIRSMethod')
+    disp('BST> Database structure: Adding NIRS method definition to headmodel structure...');
+    ApplyToDatabase(@AddNirsMethod);
+    disp('BST> Database structure: Done.');
+    % Reset all the saved options
+    bst_set('ProcessOptions', []);
+end
 
 %% ===== JUST BEFORE RETURNING TO STARTUP FUNCTION =====
 % Save the new database version

toolbox/core/bst_set.m
@@ -277,7 +277,7 @@ switch contextName
           'StatThreshOptions', 'ContactSheetOptions', 'ProcessOptions', 'BugReportOptions', 'DefaultSurfaceDisplay', ...
           'MagneticExtrapOptions', 'MriOptions', 'ConnectGraphOptions', 'NodelistOptions', 'IgnoreMemoryWarnings', 'SystemCopy', ...
           'TimefreqOptions_morlet', 'TimefreqOptions_hilbert', 'TimefreqOptions_fft', 'TimefreqOptions_psd', 'TimefreqOptions_stft', 'TimefreqOptions_plv', ...
-          'OpenMEEGOptions', 'DuneuroOptions', 'DigitizeOptions', 'PcaOptions', 'CustomColormaps', 'PluginCustomPath', 'BrainSuiteDir', 'PythonExe', ...
+          'OpenMEEGOptions', 'DuneuroOptions','NIRSTORMOptions', 'DigitizeOptions', 'PcaOptions', 'CustomColormaps', 'PluginCustomPath', 'BrainSuiteDir', 'PythonExe', ...
           'GridOptions_headmodel', 'GridOptions_dipfit', 'LastPsdDisplayFunction', 'LastTfDisplayFunction', 'KlustersExecutable', 'ExportBidsOptions', 'ShowHiddenFiles'}
         GlobalData.Preferences.(contextName) = contextValue;
 

toolbox/core/bst_startup.m
@@ -205,7 +205,7 @@ disp('BST> Loading configuration file...');
 % Get user database file : brainstorm.mat
 dbFile = bst_get('BrainstormDbFile');
 % Current DB version
-CurrentDbVersion = 5.02;
+CurrentDbVersion = 5.03;
 % Get default colormaps list
 sDefColormaps = bst_colormaps('Initialize');
 isDbLoaded = 0;

toolbox/db/db_template.m
@@ -55,7 +55,8 @@ switch lower(structureName)
                           'EEGMethod',      '', ...
                           'Comment',        '', ...
                           'ECOGMethod',     '', ...
-                          'SEEGMethod',     '');
+                          'SEEGMethod',     '', ...
+                          'NIRSMethod',     '');
     case 'results'
         template = struct('FileName',       '', ...
                           'Comment',        '', ...
@@ -159,6 +160,7 @@ switch lower(structureName)
                           'EEGMethod',     [], ...
                           'ECOGMethod',    [], ...
                           'SEEGMethod',    [], ...
+                          'NIRSMethod',    [], ...
                           'Gain',          [], ... 
                           'Comment',       '', ...  [nChannel, 3*nSources]
                           'HeadModelType', '', ...  {'volume', 'surface'}
@@ -395,7 +397,8 @@ switch lower(structureName)
                   'EEGMethod',        {}, ...
                   'Comment',          {}, ...
                   'ECOGMethod',       {}, ...
-                  'SEEGMethod',       {}), ...
+                  'SEEGMethod',       {}, ...
+                  'NIRSMethod',       {}), ...
               'iHeadModel',        [], ...
               ... repmat(db_template('Results'),0)
               'Result',              struct(...

toolbox/forward/bst_headmodeler.m
@@ -24,6 +24,7 @@ function [OPTIONS, errMessage] = bst_headmodeler(OPTIONS)
 %         - 'openmeeg'        : OpenMEEG forward model
 %     .SEEGMethod:    'openmeeg' and 'duneuro'  
 %     .ECOGMethod:    'openmeeg' and 'duneuro' 
+%     .NIRSMethod:    'import'
 %
 %     ======= METHODS OPTIONS =============================================
 %     OpenMEEG: see bst_openmeeg
@@ -85,6 +86,7 @@ Def_OPTIONS = struct(...
     'EEGMethod',          'eeg_3sphereberg', ...
     'ECOGMethod',         'openmeeg', ...
     'SEEGMethod',         'openmeeg', ...
+    'NIRSMethod',         'import', ...
     'HeadCenter',         [],...
     'Radii',              [.88 .93 1],...
     'Conductivity',       [.33 .0042 .33],...
@@ -150,6 +152,7 @@ iRef  = good_channel(OPTIONS.Channel,[],'MEG REF');
 iEeg  = good_channel(OPTIONS.Channel,[],'EEG');
 iEcog = good_channel(OPTIONS.Channel,[],'ECOG');
 iSeeg = good_channel(OPTIONS.Channel,[],'SEEG');
+iNirs = good_channel(OPTIONS.Channel,[],'NIRS');
 % If no channels available for one type: ignore method
 if isempty(iMeg)
     OPTIONS.MEGMethod = '';
@@ -172,7 +175,12 @@ if isempty(iSeeg)
 elseif isempty(OPTIONS.SEEGMethod)
     iSeeg = [];
 end
-if isempty(OPTIONS.EEGMethod) && isempty(OPTIONS.MEGMethod) && isempty(OPTIONS.ECOGMethod) && isempty(OPTIONS.SEEGMethod)
+if isempty(iNirs)
+    OPTIONS.NIRSMethod = '';
+elseif isempty(OPTIONS.NIRSMethod)
+    iNirs = [];
+end
+if isempty(OPTIONS.EEGMethod) && isempty(OPTIONS.MEGMethod) && isempty(OPTIONS.ECOGMethod) && isempty(OPTIONS.SEEGMethod) && isempty(OPTIONS.NIRSMethod)
     errMessage = 'Nothing to process...';
     OPTIONS = [];
     return;
@@ -199,12 +207,13 @@ OPTIONS.iMeg  = [iMeg iRef];
 OPTIONS.iEeg  = iEeg;
 OPTIONS.iEcog = iEcog;
 OPTIONS.iSeeg = iSeeg;
+OPTIONS.iNirs = iNirs;
     
 
 %% ===== OUTPUT FILENAME ===========================================================================
 %  =================================================================================================
 % Get all methods
-allMethods = unique({OPTIONS.MEGMethod, OPTIONS.EEGMethod, OPTIONS.ECOGMethod, OPTIONS.SEEGMethod});
+allMethods = unique({OPTIONS.MEGMethod, OPTIONS.EEGMethod, OPTIONS.ECOGMethod, OPTIONS.SEEGMethod, OPTIONS.NIRSMethod});
 allMethods(cellfun(@isempty, allMethods)) = [];
 % Build default comment
 if isempty(OPTIONS.Comment)
@@ -404,12 +413,12 @@ else
     end
 end
 % Check for any sensor located "inside" the inner skull (that shouldn't be there)
-allLoc = [OPTIONS.Channel([iMeg iEeg]).Loc];
+allLoc = [OPTIONS.Channel([iMeg iEeg, iNirs]).Loc];
 if ~isempty(allLoc)
     iVertInside = find(inpolyhd(allLoc, sSurfInner.Vertices, sSurfInner.Faces));
     % Warning if there are some sensors inside
     if ~isempty(iVertInside)
-        errMessage = ['Some EEG or MEG sensors are located inside the brain volume.' 10 ...
+        errMessage = ['Some EEG, MEG  or NIRS sensors are located inside the brain volume.' 10 ...
                       'You should check the positions of the sensors and the type of the channels.' 10 10 ...
                       'Position: Right-click on the channel file > MRI registration > Edit.' 10 ...
                       'Type: Right-click on the channel file > Edit channel file.'];
@@ -579,12 +588,69 @@ if (~isempty(OPTIONS.MEGMethod) && ~ismember(OPTIONS.MEGMethod, {'openmeeg', 'du
     end
 else
     Param = [];
-end    
-% Check for errors: NaN values in the Gain matrix
+end   
+
+
+%% ===== COMPUTE: NIRSTORM HEADMODELS =====
+if (~isempty(OPTIONS.NIRSMethod) && strcmpi(OPTIONS.NIRSMethod, {'import'}))
+    if ~strcmp(OPTIONS.HeadModelType, 'surface')
+        errMessage = sprintf('%s headmodel is not supported for NIRS', OPTIONS.HeadModelType);
+        OPTIONS = [];
+        return;
+    end
+    % Check NIRSTORM plugin
+    [isInstalled, errMsg, PlugDesc] = bst_plugin('Install', 'nirstorm');
+    if ~isInstalled
+        errMessage = ['NIRSTORM plugin is required to compute NIRS head model' 10 errMsg];
+        OPTIONS = [];
+        return;
+    end
+    % Check version, update if needed
+    if bst_plugin('CompareVersions', PlugDesc.Version, '0.9.1') < 0
+        if PlugDesc.isManaged
+            bst_plugin('Uninstall', 'nirstorm', 0, 0);
+            bst_plugin('Install',   'nirstorm');
+        end
+    end
+    % Get Subject
+    sSubject = bst_get('SurfaceFile', OutSurfaceFile);
+    voronoi_fn = process_nst_compute_voronoi('get_voronoi_fn', sSubject);
+    % Subject Informations 
+    OPTIONS.SubjectName = sSubject.Name;
+    OPTIONS.MriFile     = sSubject.Anatomy(sSubject.iAnatomy).FileName;
+    OPTIONS.VoronoiFile = voronoi_fn;
+    OPTIONS.CortexFile  = sSubject.Surface(sSubject.iCortex ).FileName;
+    % Use defined options
+    if ~isfield(OPTIONS, 'FluenceFolder') ||  ~isfield(OPTIONS, 'smoothing_method')  ||  ~isfield(OPTIONS, 'smoothing_fwhm')
+        sOptions = gui_show_dialog('NIRS headmodel options', @panel_headmodel_nirstorm);
+        if isempty(sOptions)
+            errMessage = 'Canceled by user.';
+            OPTIONS = [];
+            return;
+        end
+        OPTIONS = struct_copy_fields(OPTIONS, sOptions, 1);
+    end
+    % Compute head model
+    [gain_nirs, errMessage, warning_message] = process_nst_import_head_model('Compute', OPTIONS);
+    if  ~isempty(warning_message)
+        for iMessage = 1:length(warning_message)
+            warning(warning_message{iMessage});
+        end
+    end
+    Gain(iNirs,:) = gain_nirs(iNirs, :);
+    Param = struct('FluenceFolder',    OPTIONS.FluenceFolder , ...
+                   'smoothing_method', OPTIONS.smoothing_method, ...
+                   'smoothing_fwhm',   OPTIONS.smoothing_fwhm);
+else
+    Param = [];
+end   
+
+%% Check for errors: NaN values in the Gain matrix
 if (nnz(isnan(Gain(iEeg,:))) > 0)  && ~isempty(OPTIONS.EEGMethod)  || ...
    (nnz(isnan(Gain(iMeg,:))) > 0)  && ~isempty(OPTIONS.MEGMethod)  || ...
    (nnz(isnan(Gain(iEcog,:))) > 0) && ~isempty(OPTIONS.ECOGMethod) || ...
-   (nnz(isnan(Gain(iSeeg,:))) > 0) && ~isempty(OPTIONS.SEEGMethod)
+   (nnz(isnan(Gain(iSeeg,:))) > 0) && ~isempty(OPTIONS.SEEGMethod) || ...
+   (nnz(isnan(Gain(iNirs,:))) > 0) && ~isempty(OPTIONS.NIRSMethod)
     errMessage = ['An unknown error occurred in the computation of the head model:' 10 ...
                   'NaN values found for valid sensors in the Gain matrix'];
     OPTIONS = [];
@@ -625,6 +691,7 @@ SaveHeadModel = struct(...
     'EEGMethod',     OPTIONS.EEGMethod, ...
     'ECOGMethod',    OPTIONS.ECOGMethod, ...
     'SEEGMethod',    OPTIONS.SEEGMethod, ...
+    'NIRSMethod',    OPTIONS.NIRSMethod, ...
     'Gain',          Gain, ...                   % FT 11-Jan-10: Remove "single"
     'Comment',       OPTIONS.Comment, ...
     'HeadModelType', OPTIONS.HeadModelType, ...
@@ -645,5 +712,4 @@ end
 bst_progress('stop');
 bst_progress('removeimage');
 
-
-
+end

toolbox/forward/panel_headmodel.m
@@ -1,7 +1,7 @@
 function varargout = panel_headmodel(varargin)
 % PANEL_HEADMODEL: Computation of forward model (GUI).
 % 
-% USAGE:     bstPanel = panel_headmodel('CreatePanel',      isMeg, isEeg, isEcog, isSeeg, isMixed)
+% USAGE:     bstPanel = panel_headmodel('CreatePanel',      isMeg, isEeg, isEcog, isSeeg, isMixed, isNirs)
 %         OutputFiles = panel_headmodel('ComputeHeadModel', iStudies, sMethod)
 
 % @=============================================================================
@@ -131,13 +131,14 @@ function [bstPanelNew, panelName] = CreatePanel(isMeg, isEeg, isEcog, isSeeg, is
     % === NIRS ===
     if isNirs
         % Checkbox
-        jCheckMethodNIRS = gui_component('CheckBox', jPanelMethod, 'br', 'NIRS: ');
-        jCheckMethodNIRS.setSelected(0);
-        jCheckMethodNIRS.setEnabled(0)
-        % Label
-        gui_component('Label', jPanelMethod, 'tab hfill', ['<HTML><FONT color="#777777">To compute head model for NIRS, use process:<BR>' ...
-                                                           'NIRS > Sources > Compute head model from fluence<BR>' ...
-                                                           '(NIRSTORM plugin is required)']);
+        jCheckMethodNIRS = gui_component('CheckBox', jPanelMethod, 'br', 'NIRS: ', [], [], @UpdateComment);
+        jCheckMethodNIRS.setSelected(1);
+         % Combobox
+        jComboMethodNIRS = gui_component('ComboBox', jPanelMethod, 'tab hfill', [], [], [], @UpdateComment, []);
+        jComboMethodNIRS.addItem(BstListItem('import', '', 'Import from MCXlab', []));
+    else
+        jCheckMethodNIRS = [];
+        jComboMethodNIRS = [];
     end
     % Attach sub panel to NewPanel
     jPanelNew.add('br hfill', jPanelMethod);
@@ -163,7 +164,8 @@ function [bstPanelNew, panelName] = CreatePanel(isMeg, isEeg, isEcog, isSeeg, is
                   'jCheckMethodECOG',    jCheckMethodECOG, ...
                   'jComboMethodECOG',    jComboMethodECOG, ...
                   'jCheckMethodSEEG',    jCheckMethodSEEG, ...
-                  'jComboMethodSEEG',    jComboMethodSEEG ...
+                  'jCheckMethodNIRS',    jCheckMethodNIRS, ...
+                  'jComboMethodNIRS',    jComboMethodNIRS ...
                  );
     % Create the BstPanel object that is returned by the function
     bstPanelNew = BstPanel(panelName, jPanelNew, ctrl);
@@ -187,16 +189,32 @@ function [bstPanelNew, panelName] = CreatePanel(isMeg, isEeg, isEcog, isSeeg, is
     %% ===== UPDATE COMMENT =====
     function UpdateComment(varargin)
         % Force to have at least one modality selected
-        if isMeg && ~isEeg && ~isEcog && ~isSeeg
+        if isMeg && ~isEeg && ~isEcog && ~isSeeg && ~isNirs
             jCheckMethodMEG.setSelected(1);
-        elseif ~isMeg && isEeg && ~isEcog && ~isSeeg
+        elseif isEeg && ~isMeg && ~isEcog && ~isSeeg && ~isNirs
             jCheckMethodEEG.setSelected(1);
+        elseif isEcog && ~isMeg && ~isEeg && ~isSeeg && ~isNirs
+            jCheckMethodECOG.setSelected(1);
+        elseif isSeeg && ~isMeg && ~isEeg && ~isEcog && ~isNirs
+            jCheckMethodSEEG.setSelected(1);
+        elseif isNirs && ~isMeg && ~isEeg && ~isEcog && ~isSeeg
+            jCheckMethodNIRS.setSelected(1);
+        end
+        % Disable NIRS for other than surface head model
+        if isNirs
+            if jRadioGridVolume.isSelected() || jRadioGridMixed.isSelected()
+                jCheckMethodNIRS.setSelected(0);
+                jCheckMethodNIRS.setEnabled(0);
+            else
+                jCheckMethodNIRS.setEnabled(1);
+            end
         end
         % MEG/EEG Checkbox
         isMegSel  = isMeg && jCheckMethodMEG.isSelected();
         isEegSel  = isEeg && jCheckMethodEEG.isSelected();
         isEcogSel = isEcog && jCheckMethodECOG.isSelected();
         isSeegSel = isSeeg && jCheckMethodSEEG.isSelected();
+        isNirsSel = isNirs && jCheckMethodNIRS.isSelected();
         % MEG/EEG Combobox
         if isMeg
             jComboMethodMEG.setEnabled(isMegSel);
@@ -209,6 +227,9 @@ function [bstPanelNew, panelName] = CreatePanel(isMeg, isEeg, isEcog, isSeeg, is
         end
         if isSeeg
             jComboMethodSEEG.setEnabled(isSeegSel);
+        end
+        if isNirs
+            jComboMethodNIRS.setEnabled(isNirsSel);
         end
         % Get current methods for EEG and MEG
         allMethods = {};
@@ -223,6 +244,9 @@ function [bstPanelNew, panelName] = CreatePanel(isMeg, isEeg, isEcog, isSeeg, is
         end
         if isSeegSel
             allMethods{end+1} = char(jComboMethodSEEG.getSelectedItem.getName());
+        end
+        if isNirsSel
+            allMethods{end+1} = char(jComboMethodNIRS.getSelectedItem.getName());
         end
         allMethods = unique(allMethods);
         allMethods(cellfun(@isempty,allMethods)) = [];
@@ -291,6 +315,11 @@ function s = GetPanelContents() %#ok<DEFNU>
     else
         s.SEEGMethod = '';
     end
+    if ~isempty(ctrl.jCheckMethodNIRS) && ctrl.jCheckMethodNIRS.isSelected()
+        s.NIRSMethod = char(ctrl.jComboMethodNIRS.getSelectedItem.getType());
+    else
+        s.NIRSMethod = '';
+    end
     s.SaveFile = 1;
 end
 
@@ -306,6 +335,7 @@ end
 %         |- EEGMethod     : {'eeg_3sphereberg', 'openmeeg', ''}
 %         |- ECOGMethod    : {'openmeeg', ''}
 %         |- SEEGMethod    : {'openmeeg', ''}
+%         |- NIRSMethod    : {'import', ''}
 %         |- HeadModelType : {'volume', 'surface'}
 %         |- Comment       : String [optional]
 %         |- Interactive   : {0,1}, if 0, does everything by default and does not shoe any message
@@ -338,6 +368,7 @@ function [OutputFiles, errMessage] = ComputeHeadModel(iStudies, sMethod) %#ok<DE
     isEeg = 0;
     isEcog = 0;
     isSeeg = 0;
+    isNirs = 0;
     for i = 1:length(sStudies)
         if ~isempty(sStudies(i).Channel)
             isMeg  = any(strcmpi(sStudies(i).Channel.DisplayableSensorTypes, 'MEG'));
@@ -348,12 +379,8 @@ function [OutputFiles, errMessage] = ComputeHeadModel(iStudies, sMethod) %#ok<DE
         end
     end
     % Check that at least one modality is available
-    if ~isMeg && ~isEeg && ~isEcog && ~isSeeg
-        if isNirs
-            errMessage = ['To compute head model for NIRS, use process:' 10 'NIRS > Sources > Compute head model from fluence' 10 'NIRSTORM plugin is required'];
-        else
-            errMessage = 'No valid sensor types to estimate a head model.';
-        end
+    if ~isMeg && ~isEeg && ~isEcog && ~isSeeg && ~isNirs
+        errMessage = 'No valid sensor types to estimate a head model.';
         return;
     end
     % Check if the first subject has a "Source model" atlas
@@ -389,6 +416,9 @@ function [OutputFiles, errMessage] = ComputeHeadModel(iStudies, sMethod) %#ok<DE
     end
     if ~isSeeg || ~isfield(sMethod, 'SEEGMethod')
         sMethod.SEEGMethod = '';
+    end
+    if ~isNirs || ~isfield(sMethod, 'NIRSMethod')
+        sMethod.NIRSMethod = '';
     end
     % List all methods
     allMethods = unique({sMethod.MEGMethod, sMethod.EEGMethod, sMethod.ECOGMethod, sMethod.SEEGMethod});
@@ -467,6 +497,7 @@ function [OutputFiles, errMessage] = ComputeHeadModel(iStudies, sMethod) %#ok<DE
         OPTIONS.iEeg  = good_channel(OPTIONS.Channel, [], 'EEG');
         OPTIONS.iEcog = good_channel(OPTIONS.Channel, [], 'ECOG');
         OPTIONS.iSeeg = good_channel(OPTIONS.Channel, [], 'SEEG');
+        OPTIONS.iNirs = good_channel(OPTIONS.Channel, [], 'NIRS');
 
         % ===== BEST FITTING SPHERE =====
         % BestFittingSphere : .HeadCenter, .Radii, .Conductivity
@@ -721,6 +752,7 @@ function [OutputFiles, errMessage] = ComputeHeadModel(iStudies, sMethod) %#ok<DE
             newHeadModel.EEGMethod     = OPTIONS.EEGMethod;
             newHeadModel.ECOGMethod    = OPTIONS.ECOGMethod;
             newHeadModel.SEEGMethod    = OPTIONS.SEEGMethod;
+            newHeadModel.NIRSMethod    = OPTIONS.NIRSMethod;
             % Update Study structure
             iHeadModel = length(sStudy.HeadModel) + 1;
             sStudy.HeadModel(iHeadModel) = newHeadModel;

toolbox/gui/panel_headmodel_nirstorm.m
@@ -1,0 +1,189 @@
+function varargout = panel_headmodel_nirstorm(varargin)
+% panel_headmodel_nirstorm: Options for the construction of nirs headmodel (GUI).
+% 
+% USAGE:  bstPanelNew = panel_headmodel_nirstorm('CreatePanel')
+%                   s = panel_headmodel_nirstorm('GetPanelContents')
+
+% @=============================================================================
+% This function is part of the Brainstorm software:
+% https://neuroimage.usc.edu/brainstorm
+% 
+% Copyright (c) University of Southern California & McGill University
+% This software is distributed under the terms of the GNU General Public License
+% as published by the Free Software Foundation. Further details on the GPLv3
+% license can be found at http://www.gnu.org/copyleft/gpl.html.
+% 
+% FOR RESEARCH PURPOSES ONLY. THE SOFTWARE IS PROVIDED "AS IS," AND THE
+% UNIVERSITY OF SOUTHERN CALIFORNIA AND ITS COLLABORATORS DO NOT MAKE ANY
+% WARRANTY, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO WARRANTIES OF
+% MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, NOR DO THEY ASSUME ANY
+% LIABILITY OR RESPONSIBILITY FOR THE USE OF THIS SOFTWARE.
+%
+% For more information type "brainstorm license" at command prompt.
+% =============================================================================@
+%
+% Authors: Edouard Delaire, 2025
+%          Raymundo Cassani, 2025
+
+eval(macro_method);
+end
+
+
+%% ===== CREATE PANEL =====
+function [bstPanelNew, panelName] = CreatePanel(sProcess, sFiles)  %#ok<DEFNU>  
+    panelName = 'HeadModelNirsOptions';
+    % Java initializations
+    import java.awt.*;
+    import javax.swing.*;
+    % Create main main panel
+    jPanelNew = gui_river();
+    % Default  options
+    if nargin >= 1 && isfield(sProcess,'options') && ~isempty(sProcess.options) && isfield(sProcess.options, 'nirstorm') && ~isempty(sProcess.options.nirstorm)
+        OPTIONS = sProcess.options.nirstorm.Value;
+    else
+        OPTIONS = GetDefaultOption();
+    end
+    % Fluence data source panel
+    jPanelSource = gui_river([4,4], [3,15,10,10], 'Fluence data source');
+    jButtonGroupSrc = ButtonGroup();
+    jRadioUrl = gui_component('radio', jPanelSource, 'br', 'URL', jButtonGroupSrc, [], @(h,ev)UpdatePanel(), []);
+    jRadioDir = gui_component('radio', jPanelSource, [], 'Path', jButtonGroupSrc, [], @(h,ev)UpdatePanel(), []);
+    % Panel to hold URL or Dir panels
+    jPanelInput = java_create('javax.swing.JPanel');
+    jPanelInput.setLayout(GridBagLayout());
+    c = GridBagConstraints();
+    c.fill    = GridBagConstraints.HORIZONTAL;
+    c.weightx = 1;
+    c.weighty = 0;
+    c.gridx   = 0;
+    c.gridy   = 0;
+    % URL panel
+    jPanelUrl = gui_river([4,4], [5,5,5,5], '');
+    jTextUrl = gui_component('text', [], '', 'https://neuroimage.usc.edu/resources/nst_data/fluence/MRI__Colin27_4NIRS/', [], [], [], []);
+    jPanelUrl.add('hfill', jTextUrl);
+    % Dir panel
+    LastUsedDirs = bst_get('LastUsedDirs');
+    jPanelDir = gui_river([4,4], [5,5,5,5], '');
+    jTextDir = gui_component('text', [], '', LastUsedDirs.ImportData, [], [], [], []);
+    jPanelDir.add('hfill', jTextDir);
+    gui_component('button', jPanelDir, '', '...', [],[], @(h,ev)PickDir_Callback());
+    % Add in same place, but URL panel (default) is added at the last, so it is shown on top of Dir panel
+    jPanelInput.add(jPanelDir, c);
+    jPanelInput.add(jPanelUrl, c);
+    jPanelSource.add('br hfill', jPanelInput);
+    jPanelNew.add('br hfill', jPanelSource);
+    % Default: URL
+    jRadioUrl.setSelected(1);
+    jPanelDir.setVisible(0);
+    % Smoothing panel
+    % method
+    jPanelSmooth = gui_river([4,4], [3,15,10,10], 'Spatial smoothing');
+    jGroupRadio = ButtonGroup();
+    jRadioGeodesic = gui_component('radio', jPanelSmooth, 'br', '<HTML>Geodesic (recommended)</HTML>', jGroupRadio, [], [], []);
+    jRadioGeodesic.setSelected(strcmp(OPTIONS.smoothing_method, 'geodesic_dist'))
+    jRadioSurfstat = gui_component('radio', jPanelSmooth, 'br', '<HTML><FONT color="#777777">Before 2023 (not recommended)</FONT></HTML>', jGroupRadio, [], [], []);
+    jRadioSurfstat.setSelected(strcmp(OPTIONS.smoothing_method, 'surfstat_before_2023'))
+    gui_component('label', jPanelSmooth, 'br');
+    gui_component('label', jPanelSmooth, 'br');
+    % fwhm
+    gui_component('label', jPanelSmooth, '', 'FWHM: ', [], [], [], []);
+    jSmoothingFwhm = gui_component('text', jPanelSmooth, 'tab', num2str(OPTIONS.smoothing_fwhm), [], [], [], []);
+    gui_component('label', jPanelSmooth, '', 'mm', [], [], [], []);
+    jPanelNew.add('br hfill', jPanelSmooth);
+
+    % ===== VALIDATION BUTTONS =====
+    gui_component('button', jPanelNew, 'br right', 'Cancel', [], [], @ButtonCancel_Callback, []);
+    gui_component('button', jPanelNew, [], 'OK', [], [], @ButtonOk_Callback, []);
+
+    % ===== PANEL CREATION =====
+    % Return a mutex to wait for panel close
+    bst_mutex('create', panelName);
+    ctrl = struct('jRadioUrl',      jRadioUrl, ...
+                  'jRadioDir',      jRadioDir, ...
+                  'jTextUrl',       jTextUrl, ...
+                  'jTextDir',       jTextDir, ...
+                  'jSmoothingFwhm', jSmoothingFwhm, ...
+                  'jRadioGeodesic', jRadioGeodesic, ...
+                  'jRadioSurfstat', jRadioSurfstat );
+    bstPanelNew = BstPanel(panelName, jPanelNew, ctrl);
+
+   
+%% =================================================================================
+%  === INTERNAL CALLBACKS ==========================================================
+%  =================================================================================
+%% ===== CANCEL BUTTON =====
+    function ButtonCancel_Callback(hObject, event)
+        % Close panel without saving (release mutex automatically)
+        gui_hide(panelName);
+    end
+
+%% ===== OK BUTTON =====
+    function ButtonOk_Callback(varargin)       
+        % Release mutex and keep the panel opened
+        bst_mutex('release', panelName);
+    end
+
+%% ===== UPDATE PANEL =====
+    function UpdatePanel(varargin)
+        % Fluence data source
+        if jRadioUrl.isSelected()
+            jPanelDir.setVisible(0);
+            jPanelUrl.setVisible(1);
+        elseif jRadioDir.isSelected()
+            jPanelUrl.setVisible(0);
+            jPanelDir.setVisible(1);
+        end
+    end
+
+%% ====== PICK DIR =====
+    function PickDir_Callback()
+        LastUsedDirs = bst_get('LastUsedDirs');
+        strFluenceDir = java_getfile('open', 'Select fluence directory', LastUsedDirs.ImportData, 'single', 'dirs', {{'*'}, 'Fluence directory', 'directory'}, 0);
+        if ~isempty(strFluenceDir)
+            jTextDir.setText(strFluenceDir);
+            LastUsedDirs.ImportData = strFluenceDir;
+            bst_set('LastUsedDirs', LastUsedDirs);
+        end
+    end
+end
+
+
+
+%% =================================================================================
+%  === EXTERNAL CALLBACKS ==========================================================
+%  =================================================================================   
+%% ===== GET DEFAULT OPTIONS =====
+function s = GetDefaultOption()
+    % Use defined options
+    s = struct();
+    s.FluenceFolder    = 'https://neuroimage.usc.edu/resources/nst_data/fluence/';
+    s.smoothing_method = 'geodesic_dist';
+    s.smoothing_fwhm   = 10;
+end
+
+
+%% ===== GET PANEL CONTENTS =====
+function s = GetPanelContents() %#ok<DEFNU>
+    % Get panel controls
+    ctrl = bst_get('PanelControls', 'HeadModelNirsOptions');
+    s = GetDefaultOption();
+    % Fluence data source
+    if ctrl.jRadioUrl.isSelected()
+        s.FluenceFolder = char(ctrl.jTextUrl.getText());
+    elseif ctrl.jRadioDir.isSelected()
+        s.FluenceFolder = char(ctrl.jTextDir.getText());
+    else
+        error('You must select a source for fluence data');
+    end
+    % Smoothing method
+    if ctrl.jRadioGeodesic.isSelected()
+        s.smoothing_method = 'geodesic_dist';
+    elseif ctrl.jRadioSurfstat.isSelected()
+        s.smoothing_method = 'surfstat_before_2023';
+    else
+        error('You must select a smoothing method');
+    end
+    % Spatial smoothing
+    s.smoothing_fwhm = str2double(ctrl.jSmoothingFwhm.getText());
+end
+

commit 420403cdabbd3d89fe1898dcf84969e264f1fa53

Bugfix: DB, bug at `db_update` for the new v5.03 (2-Sep-2025)
Introduced in: b00f3d0

toolbox/db/db_update.m
@@ -359,7 +359,7 @@ end
     if ~isempty(sStudy.HeadModel)
         [sStudy.HeadModel.NIRSMethod] = deal('');
     else
-        sStudy.Data = repmat(db_template('headmodel'), 0);
+        sStudy.HeadModel = repmat(db_template('headmodel'), 0);
     end
 end
 % Modification: Add 'NIRSMethod' to the Headmodel structure