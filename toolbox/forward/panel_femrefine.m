ed4b122cb6e7b00d2c43a51f44566194aa825a74
rcassani <raymundo.cassani@gmail.com>
2025-11-26 16:51:21 -0500

    Simplify `panel_femrefine`

--- toolbox/forward/panel_femrefine.m
+++ toolbox/forward/panel_femrefine.m
@@ -23,7 +23,8 @@ function varargout = panel_femrefine(varargin)
 % For more information type "brainstorm license" at command prompt.
 % =============================================================================@
 %
-% Authors: Takfarinas Medani, 2025, adapted from panel_femcond
+% Authors: Takfarinas Medani, 2025
+%          Raymundo Cassani, 2025
 
 eval(macro_method);
 end
@@ -36,27 +37,23 @@ function [bstPanelNew, panelName] = CreatePanel(sProcess, sFiles) %#ok<DEFNU>
     import java.awt.*;
     import javax.swing.*;
 
-    % GUI CALL:  panel_femrefine('CreatePanel', OPTIONS)
+    % GUI CALL:  panel_femrefine('CreatePanel', FemFile)
     if (nargin == 1)
-        OPTIONS = sProcess;
+        FemFile = sProcess;
     % PROCESS CALL:  panel_femrefine('CreatePanel', sProcess, sFiles)
-    else % I'm not sure about this=> check with Ray
-        OPTIONS = sProcess.options.panel_femrefine.Value;
-        % Get FEM files
+    else
+        % Get FEM file
         sSubject = bst_get('Subject', sProcess.options.subjectname.Value);
         if isempty(sSubject.iFEM)
             error('No available FEM mesh file for this subject.');
         end
-        OPTIONS.FemFile = file_fullpath(sSubject.Surface(sSubject.iFEM).FileName);
+        FemFile = sSubject.Surface(sSubject.iFEM).FileName;
     end
     
     % ==== GET MESH INFO ====
     % Load tissue labels
-    FemMat = load(OPTIONS.FemFile, 'TissueLabels');
-    % Get default conductivities
-    OPTIONS.FemNames = FemMat.TissueLabels;
-    % OPTIONS.FemCond = panel_duneuro('GetDefaultCondutivity', OPTIONS.FemNames);
-    OPTIONS.MeshEdgeLength = ones(size(OPTIONS.FemNames));
+    FemMat = load(file_fullpath(FemFile), 'TissueLabels');
+    LayerNames = FemMat.TissueLabels;
     
     % ==== FRAME STRUCTURE ====
     jPanelNew = java_create('javax.swing.JPanel');
@@ -65,20 +62,13 @@ function [bstPanelNew, panelName] = CreatePanel(sProcess, sFiles) %#ok<DEFNU>
 
     % ===== FEM LAYERS =====
     jPanelLayers = gui_river([6,6], [-5,6,15,6], 'FEM tissue(s) to refine');
-        nLayers = length(OPTIONS.FemNames);
-        jRadioLayerRefine = javaArray('javax.swing.JRadioButton', nLayers);
-        jRadioLayerNoRefine = javaArray('javax.swing.JRadioButton', nLayers);
-        %jTextEdgeSize = javaArray('javax.swing.JTextField', nLayers);
+        nLayers = length(LayerNames);
+        jCheckLayerSelect = javaArray('javax.swing.JCheckBox', nLayers);
         % Loop on each layer
         for i = 1:nLayers
-            gui_component('label', jPanelLayers, 'br', [OPTIONS.FemNames{i} ':'], [], [], [], []);
-            %jTextEdgeSize(i) = gui_component('texttime', jPanelLayers, 'tab', '', [], [], [], []);
-            %gui_validate_text(jTextEdgeSize(i), [], [], {0.0001,1000,10000}, 'mm', [], OPTIONS.MeshEdgeLength(i), []);
-            %gui_component('label', jPanelLayers, 'tab', 'mm', [], [], [], []);
-            jGroupRadio = ButtonGroup();
-            jRadioLayerRefine(i) = gui_component('radio', jPanelLayers, 'tab', 'Refine', jGroupRadio, [], @(h,ev)UpdatePanel(), []);
-            jRadioLayerNoRefine(i) = gui_component('radio', jPanelLayers, 'tab', 'No Refine', jGroupRadio, [], @(h,ev)UpdatePanel(), []);
-            jRadioLayerRefine(i).setSelected(1);
+            gui_component('label', jPanelLayers, 'br', [LayerNames{i} ':'], [], [], [], []);
+            jCheckLayerSelect(i) = gui_component('checkbox', jPanelLayers, 'tab', 'Select');
+            jCheckLayerSelect(i).setSelected(1);
         end
     jPanelNew.add(jPanelLayers);
     
@@ -92,14 +82,12 @@ function [bstPanelNew, panelName] = CreatePanel(sProcess, sFiles) %#ok<DEFNU>
     % Return a mutex to wait for panel close
     bst_mutex('create', panelName);
     % Create the BstPanel object that is returned by the function
-    ctrl = struct(...'jTextEdgeSize',          jTextEdgeSize, ...
-                  'jRadioLayerRefine',      jRadioLayerRefine, ...
-                  'jRadioLayerNoRefine',    jRadioLayerNoRefine); 
-    ctrl.FemNames = OPTIONS.FemNames;
+    ctrl = struct('jCheckLayerSelect', jCheckLayerSelect);
+    ctrl.LayerNames = LayerNames;
+    ctrl.FemFile    = FemFile;
+
     % Create the BstPanel object that is returned by the function
     bstPanelNew = BstPanel(panelName, jPanelNew, ctrl);    
-    % Redraw panel
-    UpdatePanel();
 
 
 %% =================================================================================
@@ -116,27 +104,6 @@ function [bstPanelNew, panelName] = CreatePanel(sProcess, sFiles) %#ok<DEFNU>
         % Release mutex and keep the panel opened
         bst_mutex('release', panelName);
     end
-
-    %% ===== UPDATE PANEL ======
-    function UpdatePanel()
-        % FEM Layers
-        isIsotropic = false(1, nLayers);
-        for j = 1:nLayers
-            isIsotropic(j) = jRadioLayerRefine(j).isSelected();
-            % jTextEdgeSize(j).setEnabled(isIsotropic(j));
-        end
-        
-        % Get panel
-        [bstPanel iPanel] = bst_get('Panel', 'FemRefineOptions');
-        container = get(bstPanel, 'container');
-        % Re-pack frame
-        if ~isempty(container)
-            jFrame = container.handle{1};
-            if ~isempty(jFrame)
-                jFrame.pack();
-            end
-        end
-    end
 end
 
 
@@ -151,12 +118,12 @@ function s = GetPanelContents() %#ok<DEFNU>
         s = [];
         return; 
     end
-    % FEM layers
-    for i = 1:length(ctrl.jRadioLayerRefine)
-        ... s.EdgeSize(i) = str2double(char(ctrl.jTextEdgeSize(i).getText()));
-        s.LayerRefine(i) = ctrl.jRadioLayerRefine(i).isSelected();
-        s.LayerName(i) = ctrl.FemNames(i);
+    % FEM selected layers
+    for i = 1:length(ctrl.jCheckLayerSelect)
+        s.LayerSelect(i) = ctrl.jCheckLayerSelect(i).isSelected();
     end
+    s.LayerNames = ctrl.LayerNames;
+    s.FemFile    = ctrl.FemFile;
 end
 
 
