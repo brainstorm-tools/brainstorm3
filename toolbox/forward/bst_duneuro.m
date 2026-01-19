brainstorm-tools/brainstorm3

commit 7bcaeac4db84e400767a568db815a173622488cd

Add FemMeshFile path to head modeler options (#875)
Introduces the 'FemMeshFile' field to both the default options and the saved head model structure in bst_headmodeler.m, enabling easy identification of the source FEM mesh files for further (comming) analysis

toolbox/forward/bst_headmodeler.m
@@ -95,6 +95,7 @@ Def_OPTIONS = struct(...
     'HeadFile',           [], ...
     'InnerSkullFile',     [], ...
     'OuterSkullFile',     [], ...
+    'FemMeshFile',        [], ...
     'GridOptions',        [], ... 
     'GridLoc',            [], ...
     'GridOrient',         [], ...
@@ -720,6 +721,7 @@ SaveHeadModel = struct(...
     'GridAtlas',     OPTIONS.GridAtlas, ...
     'GridOptions',   GridOptions, ...
     'SurfaceFile',   file_win2unix(OutSurfaceFile), ...
+    'FemMeshFile',   file_short(OPTIONS.FemFile), ...
     'Param',         Param);
 % History: compute head model
 SaveHeadModel = bst_history('add', SaveHeadModel, 'compute', ['Compute head model: ##' OPTIONS.Comment, '##' strHistory]);

commit e7852031692f7d3b9bb847be3d0328d36a643f89

HeadModel: Use and save `OPTIONS.FemFile` as relative path
Solves bug introduced in 7bcaeac

doc/license.html
@@ -5,7 +5,7 @@
 <body alink="#fff000" link="#fff000" vlink="#fff000">
     <h4><span style="font-family: Arial Black; color: #ffffff;"><strong>THERE IS NO UNDO BUTTON!<BR>SET UP A <FONT color=red>BACKUP</FONT> OF YOUR DATABASE</strong></span></h4>
 <HR>
-<!-- LICENCE_START -->Version: 3.260116 (16-Jan-2026)<br>
+<!-- LICENCE_START -->Version: 3.260119 (19-Jan-2026)<br>
 <span style="font-style: italic;">COPYRIGHT &copy; 2000-2025
 USC &amp; McGill University.<br>
 </span>

toolbox/forward/bst_duneuro.m
@@ -102,7 +102,7 @@ end
 
 %% ===== HEAD MODEL =====
 % Load FEM mesh
-FemMat = load(cfg.FemFile);
+FemMat = load(file_fullpath(cfg.FemFile));
 % Get mesh type
 switch size(FemMat.Elements,2)
     case 4,  ElementType = 'tetrahedron';

toolbox/forward/bst_headmodeler.m
@@ -95,7 +95,7 @@ Def_OPTIONS = struct(...
     'HeadFile',           [], ...
     'InnerSkullFile',     [], ...
     'OuterSkullFile',     [], ...
-    'FemMeshFile',        [], ...
+    'FemFile',            [], ...
     'GridOptions',        [], ... 
     'GridLoc',            [], ...
     'GridOrient',         [], ...
@@ -721,7 +721,7 @@ SaveHeadModel = struct(...
     'GridAtlas',     OPTIONS.GridAtlas, ...
     'GridOptions',   GridOptions, ...
     'SurfaceFile',   file_win2unix(OutSurfaceFile), ...
-    'FemMeshFile',   file_short(OPTIONS.FemFile), ...
+    'FemMeshFile',   OPTIONS.FemFile, ...
     'Param',         Param);
 % History: compute head model
 SaveHeadModel = bst_history('add', SaveHeadModel, 'compute', ['Compute head model: ##' OPTIONS.Comment, '##' strHistory]);

toolbox/forward/panel_duneuro.m
@@ -60,7 +60,7 @@ function [bstPanelNew, panelName] = CreatePanel(sProcess, sFiles) %#ok<DEFNU>
         if isempty(sSubject.iFEM)
             error('No available FEM mesh file for this subject.');
         end
-        OPTIONS.FemFile = file_fullpath(sSubject.Surface(sSubject.iFEM).FileName);
+        OPTIONS.FemFile = sSubject.Surface(sSubject.iFEM).FileName;
     end
     % Default options
     % defOPTIONS = bst_get('DuneuroOptions');
@@ -69,7 +69,7 @@ function [bstPanelNew, panelName] = CreatePanel(sProcess, sFiles) %#ok<DEFNU>
 
     % ==== GET MESH INFO ====
     % Load tissue labels
-    FemMat = load(OPTIONS.FemFile, 'TissueLabels');
+    FemMat = load(file_fullpath(OPTIONS.FemFile), 'TissueLabels');
     % Get default conductivities
     OPTIONS.FemNames = FemMat.TissueLabels;
     OPTIONS.FemCond = GetDefaultCondutivity(OPTIONS.FemNames);
@@ -81,7 +81,7 @@ function [bstPanelNew, panelName] = CreatePanel(sProcess, sFiles) %#ok<DEFNU>
         OPTIONS.FemSelect = ones(size(OPTIONS.FemCond));
     end
     % Get size of Tensors matrix
-    Tensors = whos('-file', OPTIONS.FemFile, 'Tensors');
+    Tensors = whos('-file', file_fullpath(OPTIONS.FemFile), 'Tensors');
     OPTIONS.UseTensor = (~isempty(Tensors) && all(Tensors.size > 0));
     
     % ==== FRAME STRUCTURE ====

toolbox/forward/panel_femcond.m
@@ -47,12 +47,12 @@ function [bstPanelNew, panelName] = CreatePanel(sProcess, sFiles) %#ok<DEFNU>
         if isempty(sSubject.iFEM)
             error('No available FEM mesh file for this subject.');
         end
-        OPTIONS.FemFile = file_fullpath(sSubject.Surface(sSubject.iFEM).FileName);
+        OPTIONS.FemFile = sSubject.Surface(sSubject.iFEM).FileName;
     end
     
     % ==== GET MESH INFO ====
     % Load tissue labels
-    FemMat = load(OPTIONS.FemFile, 'TissueLabels');
+    FemMat = load(file_fullpath(OPTIONS.FemFile), 'TissueLabels');
     % Get default conductivities
     OPTIONS.FemNames = FemMat.TissueLabels;
     OPTIONS.FemCond = panel_duneuro('GetDefaultCondutivity', OPTIONS.FemNames);

toolbox/forward/panel_headmodel.m
@@ -686,7 +686,7 @@ function [OutputFiles, errMessage] = ComputeHeadModel(iStudies, sMethod) %#ok<DE
                 errMessage = 'No FEM head model available for this subject.';
                 return;
             end
-            OPTIONS.FemFile = file_fullpath(sSubject.Surface(sSubject.iFEM(1)).FileName);
+            OPTIONS.FemFile = sSubject.Surface(sSubject.iFEM(1)).FileName;
             % Interactive interface to set the OpenMEEG options
             if OPTIONS.Interactive
                 DuneuroOptions = gui_show_dialog('DUNEuro options', @panel_duneuro, 1, [], OPTIONS);

toolbox/gui/panel_femname.m
@@ -49,12 +49,12 @@ function [bstPanelNew, panelName] = CreatePanel(sProcess, sFiles)
         if isempty(sSubject.iFEM)
             error('No available FEM mesh file for this subject.');
         end
-        OPTIONS.FemFile = file_fullpath(sSubject.Surface(sSubject.iFEM).FileName);
+        OPTIONS.FemFile = sSubject.Surface(sSubject.iFEM).FileName;
     end
     
     % ==== GET MESH INFO ====
     % Load tissue labels
-    FemMat = load(OPTIONS.FemFile, 'TissueLabels');
+    FemMat = load(file_fullpath(OPTIONS.FemFile), 'TissueLabels');
     OldLabels = FemMat.TissueLabels;
 
     % ==== FRAME STRUCTURE ====