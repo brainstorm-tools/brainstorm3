c0e2a117d2aa33ddc4d56c217362020d56601149
tmedani <takfarinesmedani@live.fr>
2026-01-29 12:46:47 -0800

    Move FEM layer extraction into process_fem_mesh
    Remove standalone extract_femlayers.m and embed its logic as ExtractFemlayers in process_fem_mesh. Update panel ID from 'FemRefineOptions' to 'FemSelectLayer' and adjust GetPanelContents accordingly. Update tree menu entries and callbacks: relabel menu items to 'Extract FEM surfaces' and 'Extract FEM volumes', and call process_fem_mesh('ExtractFemlayers', ...) for volume extraction. The new function respects dialog cancellation, saves the new FEM file, and adds it to the database.

--- toolbox/process/functions/process_fem_mesh.m
+++ toolbox/process/functions/process_fem_mesh.m
@@ -6,6 +6,7 @@ function varargout = process_fem_mesh( varargin )
 %                OPTIONS = process_fem_mesh('GetDefaultOptions')
 %                  label = process_fem_mesh('GetFemLabel', label)
 %             NewFemFile = process_fem_mesh('SwitchHexaTetra', FemFile)
+%             NewFemFile = process_fem_mesh('ExtractFemlayers', FemFile)
 %  [sSubject, T1File, T2File, errMsg] = process_fem_mesh('GetT1T2', iSubject, iMris=[])
 
 % @=============================================================================
@@ -1572,4 +1573,59 @@ function NewFemFile = SwitchHexaTetra(FemFile) %#ok<DEFNU>
     elseif (elemSize.size(2) == 4)
         NewFemFile = fem_tetra2hexa(FemFullFile);
     end
+end
+
+function NewFemFile = ExtractFemlayers(FemFile)
+    % Ask user to select the layer to refine with panel_femselect
+    OPTIONS = gui_show_dialog('Refine FEM mesh', @panel_femselect, 1, [], FemFile);
+    if isempty(OPTIONS)
+        return;
+    end
+    % Load FEM mesh
+    bst_progress('start', 'Convert FEM mesh', ['Loading file: "' FemFile '"...']);
+    FemFile = file_fullpath(FemFile);
+    FemMat = load(FemFile);
+    % Get tissues marked
+    selectedTissue = find(OPTIONS.LayerSelect);
+    selectedElementIndex = [];
+    tissueId = [];
+    tissueLabel = {};
+    if ~isempty(selectedTissue)
+        for iTissue = 1 : length(selectedTissue)
+            tmpIndx = find(FemMat.Tissue == selectedTissue(iTissue));
+            selectedElementIndex = [selectedElementIndex; tmpIndx];
+            tissueId = [tissueId; repmat(iTissue, length(tmpIndx),1)];
+            tissueLabel{iTissue} =  FemMat.TissueLabels{selectedTissue(iTissue)};
+        end
+    end
+    NewElem = FemMat.Elements(selectedElementIndex, :);
+    [NewNode, NewElem] = removeisolatednode(FemMat.Vertices, [NewElem tissueId]);
+    % figure; plotmesh(NewNode, NewElem, 'x>0')
+    FemMat.Vertices = NewNode;
+    FemMat.Elements = NewElem(:, 1:4);
+    FemMat.Tissue = tissueId;
+    FemMat.TissueLabels = tissueLabel;
+    % Edit file comment: number of layers
+    oldNlayers = regexp(FemMat.Comment, '\d+ layers', 'match');
+    if ~isempty(oldNlayers)
+        FemMat.Comment = strrep(FemMat.Comment, oldNlayers{1}, sprintf('%d layers', length(FemMat.TissueLabels)));
+    else
+        FemMat.Comment = sprintf('%s (%d layers)', str_remove_parenth(FemMat.Comment), length(FemMat.TissueLabels));
+    end
+    % Edit file comment: number of nodes
+    oldNvert = regexp(FemMat.Comment, '\d+V', 'match');
+    if ~isempty(oldNvert)
+        FemMat.Comment = strrep(FemMat.Comment, oldNvert{1}, sprintf('%dV', size(FemMat.Vertices, 1)));
+    end
+    % Output filename
+    [fPath, fBase, fExt] = bst_fileparts(FemFile);
+    NewFile = file_unique(bst_fullfile(fPath, [fBase, '_merge', fExt]));
+    % Save new surface in Brainstorm format
+    bst_progress('text', 'Saving new mesh...');
+    bst_save(NewFile, FemMat, 'v7');
+    % Add to database
+    [sSubject, iSubject] = bst_get('SurfaceFile', FemFile);
+    db_add_surface(iSubject, NewFile, FemMat.Comment);
+    % Close progress bar
+    bst_progress('stop');
 end
