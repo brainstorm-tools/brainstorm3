a921eeb9223750155b6709c09aea65167eaa08dd
Edouard2laire <edouard.delaire@icloud.com>
2026-02-16 14:23:10 -0500

    Process: Better check for non existing process file. Add check for read permission (#886)
    * Better check for non existing file / broken sym links
    
    Avoid issue with broken symbolic links, and file without reading permissions.
    
    * Use file_exist tool instead of exist
    
    * Reorganize, so error message for fileread() includes the process type
    
    ---------
    
    Co-authored-by: rcassani <raymundo.cassani@gmail.com>

--- toolbox/process/panel_process_select.m
+++ toolbox/process/panel_process_select.m
@@ -2967,7 +2967,7 @@ function ParseProcessFolder(isForced) %#ok<DEFNU>
     end
     % Add plugin processes to list of processes
     if ~isempty(plugFunc)
-        iFunc    = cellfun(@(x)exist(x,'file') > 0 , plugFunc);
+        iFunc    = cellfun(@(x)file_exist(x), plugFunc);
         plugList = cellfun(@dir, plugFunc(iFunc));
         bstFunc  = union(plugFunc, bstFunc);
     end
@@ -3024,38 +3024,45 @@ function ParseProcessFolder(isForced) %#ok<DEFNU>
                 isChangeDir = 1;
             end
         end
+        % Check that process file can be read
+        try
+            txt = fileread([fName fExt]);
+        catch
+            errMsg = 'Unable to open file';
+        end
         % Check presence of required functions in process file
-        reqFncs = {'GetDescription', 'FormatComment', 'Run'};
-        reqFncsMissing = [];
-        txt = fileread([fName fExt]);
-        for iReqFnc = 1 : length(reqFncs)
-            expression = ['^ *function.*[ |=]' reqFncs{iReqFnc} '\('];
-            res = regexp(txt, expression, 'match', 'lineanchors', 'dotexceptnewline');
-            if isempty(res)
-                reqFncsMissing(end+1) = iReqFnc;
-            end
-        end
-        if ~isempty(reqFncsMissing)
-            errMsg = 'Missing function';
-            if length(reqFncsMissing) == 1
-                errMsg = [errMsg, ': ', reqFncs{reqFncsMissing}];
-            else
-                errMsg = [errMsg, 's: ', strjoin(reqFncs(reqFncsMissing), ', ')];
-            end
-        end
-        % Get function handle
-        Function = str2func(fName);
-        % Restore previous dir
-        if isChangeDir
-            cd(curDir);
-        end
-        % Call description function
         if isempty(errMsg)
+            reqFncs = {'GetDescription', 'FormatComment', 'Run'};
+            reqFncsMissing = [];
+            for iReqFnc = 1 : length(reqFncs)
+                expression = ['^ *function.*[ |=]' reqFncs{iReqFnc} '\('];
+                res = regexp(txt, expression, 'match', 'lineanchors', 'dotexceptnewline');
+                if isempty(res)
+                    reqFncsMissing(end+1) = iReqFnc;
+                end
+            end
+            if ~isempty(reqFncsMissing)
+                errMsg = 'Missing function';
+                if length(reqFncsMissing) == 1
+                    errMsg = [errMsg, ': ', reqFncs{reqFncsMissing}];
+                else
+                    errMsg = [errMsg, 's: ', strjoin(reqFncs(reqFncsMissing), ', ')];
+                end
+            end
+        end
+        % Check call to GetDescription function
+        if isempty(errMsg)
+            % Get function handle
+            Function = str2func(fName);
             try
                 desc = Function('GetDescription');
             catch
                 errMsg = 'Could not run GetDescription()';
             end
+        end
+        % Restore previous dir
+        if isChangeDir
+            cd(curDir);
         end
         % Report error and skip process
         if ~isempty(errMsg)
