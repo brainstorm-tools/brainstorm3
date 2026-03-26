7329089eda3656b1a213b6ba7d4d6c8369844851
Edouard <edouard.delaire@icloud.com>
2026-03-26 15:10:59 -0400

    fix loading of nk files

--- toolbox/io/in_fopen_nk.m
+++ toolbox/io/in_fopen_nk.m
@@ -442,7 +442,7 @@ sFile.header = hdr;
 % Acquisition date
 sFile.acq_date = str_date(hdr.startdate);
 % Timestamp for 0s
-if isfield(sFile.hdr, 'ctl') && isfield(sFile.hdr.ctl, 'data')
+if isfield(sFile, 'hdr') && isfield(sFile.hdr, 'ctl') && isfield(sFile.hdr.ctl, 'data')
     acq_date = datetime(sFile.acq_date, 'InputFormat','dd-MMM-yyyy', 'Locale', 'en_US');
     sFile.t0 = str_datetime(acq_date + seconds(sFile.header.ctl(1).data(1).timestamp));
 end
